7b8e362a93c72db8c803f89e840b243100bfa2ce
iamslash <iamslash@gmail.com>
2016-03-03 22:13:28 +0900

    deleted comments

--- algospot.com/BOGGLE/a.cpp
+++ algospot.com/BOGGLE/a.cpp
@@ -1,50 +1,7 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
-#include <sstream>      
-#include <iomanip>
 #include <string>
-#include <vector>
-#include <functional>
-#include <limits>
-
-//...
 
 using namespace std;
-
-// // dump functions
-// void dump_board(char board[][6])
-// {
-//     for(int i=0; i<5; ++i)
-//         cout << board[i] << endl;
-// }
-
-// void dump_words(int C, int N[], char words[][10][10])
-// {
-//     for(int i=0; i<C; ++i)
-//     {
-//         for(int j=0; j<N[i]; ++j)
-//         {
-//             cout << words[i][j] << endl;
-//         }
-//     }
-// }
-
-// void dump_result(int C, int N[], char words[][10][11], bool result[][10])
-// {
-//     for(int i=0; i<C; ++i)
-//     {
-//         for(int j=0; j<N[i]; ++j)
-//         {
-//             cout << words[i][j]
-//                  << " "
-//                  << (result[i][j] ? "YES" : "NO")  
-//                  << endl;
-//         }
-//     }
-// }
 
 bool is_found_syllable(char board[][6], char word[], int r, int c, int idx)
 {
@@ -98,7 +55,6 @@ int main() {
         for (int i=0; i<5; ++i)
             scanf("%s", ar_board[i]);
 
-        // dump_board(ar_board);
         scanf("%d", &N);
         
         for (int n=0; n<N; ++n)
@@ -115,8 +71,6 @@ int main() {
                  << endl;
         }       
     }
-
-    // dump_result(C, ar_N, ar_words, ar_found);
   
     return 0;
 }
