90e70154b60bd09ca9383d3c6a588468900d23c9
David Sun <iamslash@gmail.com>
2016-03-27 23:30:25 +0900

    finished base condition

--- algospot.com/BOARDCOVER/a.c
+++ algospot.com/BOARDCOVER/a.c
@@ -16,8 +16,43 @@ void dump(char boards[][20], int H, int W)
     printf("=====\n");
 }
 
-int result(char boards[][20], int H, int W)
+int get_white_cnt(char boards[][20], int H, int W)
 {
+    int r = 0;
+    
+    for (int i=0; i<H; ++i)
+    {
+        for (int j=0; j<W; ++j)
+        {
+            if (boards[i][j] == '.')
+                r++;
+        }
+    }
+
+    return r;
+}
+
+int result(char boards[][20], int H, int W, int h, int w, int n_white)
+{
+    // base condition
+
+    // there is no way 
+    if (n_white % 3 != 0)
+        return 0;
+    // found a way
+    if (n_white == 0)
+        return 1;
+    // exceed dimension without finding a way
+    if (h>=H && w>=W)
+        return 0;
+    //
+    char b = boards[h][w];
+    if (b == '#' || b == '+') return 0;
+
+    // recursion
+    
+    
+    
     return 0;
 }
 
@@ -41,7 +76,9 @@ int main() {
         }
         /* dump(boards, H, W); */
 
-        printf("%d\n", result(boards, H, W));
+        printf("%d\n",
+               result(boards, H, W, 0, 0,
+                      get_white_cnt(boards, H, W)));
     }
   
     return 0;
