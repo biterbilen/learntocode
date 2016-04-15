0407c0ba77b34f63d5064822975d5d445799d1fa
David Sun <iamslash@gmail.com>
2016-04-15 11:33:11 +0900

    finished failed

--- algospot.com/FENCE/a.cpp
+++ algospot.com/FENCE/a.cpp
@@ -4,27 +4,44 @@
 
 using namespace std;
 
-int get_max_square_size(vector<int> & H, int l, int r)
+int get_max_square_size(vector<int> & H, int l, int cnt)
 {
+    printf("[%d, %d]\n", l, cnt);
+    
     int max_size = 0; // return value
     // base condition
-    if (l >= r) return H[l];
+    if (cnt == 1) return H[l];
 
     // recursion
-    int idx_mid = r/2;
+    int idx_mid = l + cnt/2;
+    int cnt_part_1 = cnt % 2 == 0 ? cnt / 2 : cnt / 2 + 1;
+    int cnt_part_2 = cnt/ 2;
     // get left, right size
-    max_size = get_max_square_size(H, l, idx_mid);
-    max_size = max(max_size, get_max_square_size(H, idx_mid+1, r));
+    max_size = get_max_square_size(H, l, cnt_part_1);
+    max_size = max(max_size, get_max_square_size(H, idx_mid+1, cnt_part_2));
  
     // get middle size
     int l_inner = idx_mid;
-    int r_inner = idx_mid;
-    while ( l_inner >= l && r_inner <= r )
+    int r_inner = idx_mid+1;
+    int max_size_inner = 0;
+    
+    while ( l_inner >= l && r_inner <= l+cnt-1 )
     {
+        int cnt_inner = r_inner - l_inner + 1;
         
+        if (H[l_inner] > H[r_inner])
+        {   
+            max_size_inner = max(max_size_inner, cnt_inner * H[r_inner]);
+            r_inner++;
+        }
+        else
+        {
+            max_size_inner = max(max_size_inner, cnt_inner * H[l_inner]);
+            l_inner--;
+        }
     }
     
-    return max_size;
+    return max(max_size, max_size_inner);
 }
 
 int main() {
@@ -45,7 +62,7 @@ int main() {
             H.push_back(h);
         }
 
-        printf("%d\n", get_max_square_size(H, 0, H.size()-1));
+        printf("%d\n", get_max_square_size(H, 0, H.size()));
     }
   
     return 0;
