643e8970a79b0e76623b4c9a639e7c62842015a6
David Sun <iamslash@gmail.com>
2016-04-16 08:51:36 +0900

    FENCE failed

--- algospot.com/FENCE/a.cpp
+++ algospot.com/FENCE/a.cpp
@@ -18,70 +18,30 @@ int get_max_square_size(vector<int> & H, int l, int r)
     max_size = get_max_square_size(H, l, idx_mid);
     max_size = max(max_size, get_max_square_size(H, idx_mid+1, r));
 
-
     // get middle max size
     int l_inner = idx_mid;
     int r_inner = idx_mid+1;
-    int max_size_inner = 0;
+    int h_min = min(H[l_inner], H[r_inner]);;
+    int max_size_inner = 2 * h_min;
 
-    while ( l_inner >= l && r_inner <= r )
+    while ( l_inner > l || r_inner < r )
     {
-        int cnt_inner = r_inner - l_inner + 1;
-        
-        if (H[l_inner] > H[r_inner])
-        {   
-            max_size_inner = max(max_size_inner, cnt_inner * H[r_inner]);
+        if (r_inner < r && (l_inner == l || H[l_inner-1] < H[r_inner-1] ))
+        {
             r_inner++;
+            h_min = min(h_min, H[r_inner]);
         }
         else
         {
-            max_size_inner = max(max_size_inner, cnt_inner * H[l_inner]);
             l_inner--;
+            h_min = min(h_min, H[l_inner]);            
         }
+        max_size_inner = max(max_size_inner, (r_inner - l_inner + 1) * h_min);
+        printf("=>[%d, %d] %d * %d\n", l_inner, r_inner, r_inner - l_inner + 1, h_min);
     }
     
     return max(max_size, max_size_inner);
 }
-
-// int get_max_square_size(vector<int> & H, int l, int cnt)
-// {
-//     printf("[%d, %d]\n", l, cnt);
-    
-//     int max_size = 0; // return value
-//     // base condition
-//     if (cnt == 1) return H[l];
-
-//     // recursion
-//     int idx_mid = l + cnt/2;
-//     int cnt_part_1 = cnt % 2 == 0 ? cnt / 2 : cnt / 2 + 1;
-//     int cnt_part_2 = cnt/ 2;
-//     // get left, right size
-//     max_size = get_max_square_size(H, l, cnt_part_1);
-//     max_size = max(max_size, get_max_square_size(H, idx_mid+1, cnt_part_2));
- 
-//     // get middle size
-//     int l_inner = idx_mid;
-//     int r_inner = idx_mid+1;
-//     int max_size_inner = 0;
-    
-//     while ( l_inner >= l && r_inner <= l+cnt-1 )
-//     {
-//         int cnt_inner = r_inner - l_inner + 1;
-        
-//         if (H[l_inner] > H[r_inner])
-//         {   
-//             max_size_inner = max(max_size_inner, cnt_inner * H[r_inner]);
-//             r_inner++;
-//         }
-//         else
-//         {
-//             max_size_inner = max(max_size_inner, cnt_inner * H[l_inner]);
-//             l_inner--;
-//         }
-//     }
-    
-//     return max(max_size, max_size_inner);
-// }
 
 int main() {
     
