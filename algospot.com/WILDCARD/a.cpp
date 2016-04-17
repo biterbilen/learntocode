3230eaa17d0bfe23d4117dd9ee309616736535cf
David Sun <iamslash@gmail.com>
2016-04-17 10:23:11 +0900

    wrong answer

--- algospot.com/WILDCARD/a.cpp
+++ algospot.com/WILDCARD/a.cpp
@@ -2,22 +2,28 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-bool is_match(string & W, string & F, int w, int f)
+bool is_match(int C[][101], string & W, string & F, int w, int f)
 {
-    printf("W:%s F:%s w:%d f:%d\n", W.c_str(), F.c_str(), w, f);
+    // printf("W:%s F:%s w:%d f:%d\n", W.c_str(), F.c_str(), w, f);
+
+    // retrieve cache
+    int & r = C[w][f];
+    if (r >= 0) return r;
     
-    // base condition
-    // if (w >= W.size() && f >= F.size()) return true;
-    if (f >= F.size()) return true;
+    // base condition 
+    if (w >= W.length() && f >= F.length()) return r = true;
     // recursion
-    if (W[w] == '*')
-        return is_match(W, F, w, f+1) ||
-            is_match(W, F, w+1, f+1);
-    else if (W[w] == '?' || W[w] == F[f])
-        return is_match(W, F, w+1, f+1);
+    if (W[w] == '?' || W[w] == F[f])
+        return r = is_match(C, W, F, w+1, f+1);
+    else if (W[w] == '*' && f >= F.length())
+        return r = is_match(C, W, F, w+1, f);
+    else if (W[w] == '*')
+        return (r = is_match(C, W, F, w, f+1)) ||
+            (r = is_match(C, W, F, w+1, f+1));
     
     return false;
 }
@@ -37,11 +43,12 @@ int main() {
         for (int n=0; n<N; ++n)
         {
             string F;
-            cin >> F;
-            if (is_match(W, F, 0, 0))
+            cin >> F; 
+            int C[101][101];
+            memset(C, -1, sizeof(C));
+            if (is_match(C, W, F, 0, 0))
                 printf("%s\n", F.c_str());
         }
-
     }
   
     return 0;
