af056c4780dc4ad718cd7339e09e577c8275f0d8
David Sun <iamslash@gmail.com>
2016-04-17 10:35:45 +0900

    wrong answer

--- algospot.com/WILDCARD/a.cpp
+++ algospot.com/WILDCARD/a.cpp
@@ -8,23 +8,26 @@ using namespace std;
 
 bool is_match(int C[][101], string & W, string & F, int w, int f)
 {
-    // printf("W:%s F:%s w:%d f:%d\n", W.c_str(), F.c_str(), w, f);
+    printf("W:%s F:%s w:%d f:%d\n", W.c_str(), F.c_str(), w, f);
 
     // retrieve cache
     int & r = C[w][f];
     if (r >= 0) return r;
-    
-    // base condition 
-    if (w >= W.length() && f >= F.length()) return r = true;
+
     // recursion
-    if (W[w] == '?' || W[w] == F[f])
+    // case of '?', character not '*'
+    if (w < W.size() && f < F.size() &&
+           (W[w] == '?' || W[w] == F[f] ))
+    {
         return r = is_match(C, W, F, w+1, f+1);
-    else if (W[w] == '*' && f >= F.length())
-        return r = is_match(C, W, F, w+1, f);
-    else if (W[w] == '*')
-        return (r = is_match(C, W, F, w, f+1)) ||
-            (r = is_match(C, W, F, w+1, f+1));
-    
+    } else if (W[w] == '*')
+    {
+        if ( (f < F.size() && is_match(C, W, F, w, f+1)) ||
+             is_match(C, W, F, w+1, f+1))
+            return r = true;
+    }
+
+    // base condition
     return false;
 }
 
