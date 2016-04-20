1f494bd9e4ea502456328e9e42ce60ffffcc5ca7
David Sun <iamslash@gmail.com>
2016-04-20 10:57:56 +0900

    submitted but wrong answer

--- algospot.com/TRIANGLEPATH/a.cpp
+++ algospot.com/TRIANGLEPATH/a.cpp
@@ -7,34 +7,59 @@
 
 using namespace std;
 
-int get_max_path_cnt(int B[][100], int C[][100], int N)
+void dump(int B[][100], int N)
 {
-   return 0;
+    for (int y=0; y<N; ++y)
+    {
+        for (int x=0; x<N; ++x)
+        {
+            printf("%d ", B[y][x]);
+        }
+        printf("\n");
+    }
+    printf("----------------\n");
+}
+
+int get_max_path_cnt(int B[][100], int C[][100], int N, int y, int x)
+{
+    // printf("[%d][%d] %d\n", y, x, B[y][x]);
+    
+    // check cache
+    int & r = C[y][x];
+    if (r >= 0) return r;
+    
+    // base condition
+    if (y >= N) return r = B[y][x]; 
+
+    // recursion
+    return r = B[y][x] + max(get_max_path_cnt(B, C, N, y+1, x),
+                             get_max_path_cnt(B, C, N, y+1, x+1));
 }
 
 int main() {
     
     int C; // number of cases
     scanf("%d", &C);
-
     
     for(int c=0; c<C; ++c)
     {
         int N;
         int B[100][100];
-        int C[100][100];
-        memset(C, -1, sizeof(C));
+        int CACHE[100][100];
+        memset(CACHE, -1, sizeof(CACHE));
         scanf("%d", &N);
 
         for (int n=0; n<N; ++n)
         {
-           for (int i=0; i<n; ++i)
-           {
-              scanf("%d", &B[n][i]);
-           }
+            for (int i=0; i<=n; ++i)
+            {
+                scanf("%d", &B[n][i]);
+            }
         }
 
-        printf("%d\n", get_max_path_cnt(B, C, N));
+        //dump(B, N);
+        
+        printf("%d\n", get_max_path_cnt(B, CACHE, N, 0, 0));
    }
   
     return 0;
