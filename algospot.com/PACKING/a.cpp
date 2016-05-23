bd1e4231961ecd4c1487ed229d680bcef46d7e27
David Sun <iamslash@gmail.com>
2016-05-24 07:46:21 +0900

    PACKING kick off

---
+++ algospot.com/PACKING/a.cpp
@@ -0,0 +1,79 @@
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <limits>
+#include <cstddef>
+
+const int INF = 987654321;
+
+using namespace std;
+
+int C;// case
+int N, W; // 개수, 용량
+char I_NAME[100][20] = {0,}; // 이름
+int I_WEIGHT[100] = {0,}; // 무게
+int I_NEED[100] = {0,}; // 절박도
+
+int CACHE[1001][101] = {0,}; // 용량, 서수
+
+void dump(int A[], int N)
+{
+    for (int i=0; i<N; ++i)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+
+void dump(char A[][20], int N)
+{
+    for (int i=0; i<N; ++i)
+        printf("%s ", A[i]);
+    printf("\n");
+}
+
+// return: 절박도
+int get_result(int capacity, int idx)
+{
+    printf("%d %d\n", capacity, idx);
+    
+    // base condition
+    if (idx >= N) return 0;
+    if (capacity <= 0) return 0;
+
+    // memoization
+    int & r = CACHE[capacity][idx];
+    if (r >= 0) return r;
+
+    // recursion
+    // 이번 물건을 삽입한 경우 삽입하지 않은 경우
+    r = max(get_result(capacity, idx+1),
+            get_result(capacity - I_WEIGHT[idx],
+                       idx+1) + I_NEED[idx]);
+    
+    return r;
+}
+
+int main() {
+    
+    scanf("%d", &C);
+    
+    for(int c=0; c<C; ++c)
+    {
+        memset(CACHE, -1, sizeof(CACHE));
+
+        scanf("%d", &N);
+        scanf("%d", &W);
+
+        for(int i=0; i<N; ++i)
+            scanf("%s %d %d", I_NAME[N], &I_WEIGHT[N], &I_NEED[N]);
+
+        // dump(I_WEIGHT, N);
+        // dump(I_NAME, N);
+        
+        // printf("%d\n", get_result(W, 0));
+    }
+  
+    return 0;
+}
--- algospot.com/SNAIL/a.cpp
+++ algospot.com/SNAIL/a.cpp
@@ -9,26 +9,29 @@
         
 using namespace std;
 
+int N, M; // 거리, 
+double CACHE[1000][2001]; // days, climbed
 
-
-float get_result(float C[][2001], int N, int M, int days, int climbed)
+double get_result(double C[][2001], int N, int M, int days, int climbed)
 {
     // base condition
     if (days == M + 1)
     {
         if (climbed >= N)
-            return 0.75;
+            return 1.0;
         else
-            return 0.25;
+            return 0.0;
     }
     
     // memoization
-    float & r = C[days][climbed];
-    if (r >= 0) return r;
+    double & r = C[days][climbed];
+    if (r >= -0.5) return r;
 
+    // printf("%d %d\n", days, climbed);
+    
     // recursion
-    return r = 0.75 * get_result(C, N, M, days+1, climbed + 1) +
-        0.25 * get_result(C, N, M, days+1, climbed + 2);
+    return r = (double)0.75 * get_result(C, N, M, days+1, climbed + 2) +
+        (double)0.25 * get_result(C, N, M, days+1, climbed + 1);
 }
 
 int main() {
@@ -38,15 +41,12 @@ int main() {
     
     for(int c=0; c<C; ++c)
     {
-        int N, M;
-        float CACHE[1000][2001] = {0,}; // days, climbed
-        memset(CACHE, -1, sizeof(CACHE));
+        memset(CACHE, -1.0, sizeof(CACHE));
 
         scanf("%d", &N);
         scanf("%d", &M);
-
         printf("%0.10f\n", get_result(CACHE, N, M, 1, 0));
-   }
+    }
   
     return 0;
 }
