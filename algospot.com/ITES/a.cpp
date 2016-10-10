63eca8b2fad9a79fca296299ac90f6a31442436c
iamslash <iamslash@gmail.com>
2016-10-10 17:32:59 +0900

    ITES solved

--- algospot.com/ITES/a.cpp
+++ algospot.com/ITES/a.cpp
@@ -15,33 +15,36 @@
 #include <stack>
 
 int N, K;
-const int64_t MOD = 1 << 32;
+const int64_t MOD = 4294967296;//1 << 32;x
+int CUR_A_IDX = 0;
+int64_t CUR_A_VAL = 1983;
+ 
+int64_t get_next_signal() {
+  ++CUR_A_IDX;
 
-void print_vector(const std::vector<int>& v) {
-  printf("<-- ");
-  for (int i = 0; i < v.size(); ++i) {
-    printf("%d ", v[i]);
-  }
-  printf("-->\n");
-}
-
-int64_t get_A(int i) {
-  int64_t r   = 1983;
-
-  if (i > 0)
-  {
-    r = (get_A(i-1) * 214013 + 2531011) % MOD;
-  }
-  
-  return r;
-}
-
-int64_t get_input_signal(int i) {
-  return get_A(i-1) % 10000 + 1;
+  if (CUR_A_IDX > 1)
+    CUR_A_VAL = (CUR_A_VAL * 214013 + 2531011) % MOD;
+  return CUR_A_VAL % 10000 + 1;
 }
 
 int solve() {
-  return 0;
+  int r = 0;
+  int64_t psum = 0;
+  std::queue<int64_t> psums;
+  
+  for (int i = 0; i < N; ++i) {
+    // tail을 한 칸 뒤로 이동
+    psum += get_next_signal();
+    psums.push(psum);
+    // psum[head] + k >= psum[tail]을 만족할 때까지 head를 이동
+    while (psums.front() + K < psum) {
+      psums.pop();
+    }
+    // 답을 하나 찾은 경우
+    if (psums.front() + K == psum)
+      ++r;
+  }
+  return r;
 }
 
 int main() {
@@ -57,3 +60,11 @@ int main() {
   }
   return 0;
 }
+
+
+// int main() {
+//   for (int i = 0; i < 10; ++i) {
+//     printf("%d ", get_next_signal());
+//   }
+//   return 0;
+// }
