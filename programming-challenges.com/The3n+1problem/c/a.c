1419bf592e15b83896867695f66b90e7ec93fa5d
iamslash <iamslash@gmail.com>
2016-10-18 23:21:55 +0900

    The 3n+1 problem ing...

--- programming-challenges.com/The3n+1problem/c/a.c
+++ programming-challenges.com/The3n+1problem/c/a.c
@@ -1,26 +1,41 @@
 /* Copyright (C) 2016 by iamslash */
 
 #include <stdio.h>
-#define max(a,b)
-  ({ __typeof__ (a) _a = (a);
-    __typeof__ (b) _b = (b);
-    _a > _b ? _a : _b; })
+#include <memory.h>
+
+#define max(a, b) a > b ? a : b;
 
 int CACHE[100000];
 
 int cycle_len(int n) {
-  return n;
+  // base condition
+  if (n == 1)
+    return 1;
+
+  // memoization
+  int* r = &CACHE[n];
+
+  // recursion
+  if ((*r) < 0) {
+    if (n % 2 == 0) {
+      *r = 1 + cycle_len(n / 2);
+    } else {
+      *r = 1 + cycle_len(3 * n + 1);
+    }
+  }
+  //
+  return *r;
 }
 
 int main() {
   int I, J;
-
   while (scanf("%d %d", &I, &J) != EOF) {
-    int max_cycle_len = 0;
+    memset(CACHE, -1, sizeof(CACHE));
+    int m = 0;
     for (int i = I; i <= J; ++i) {
-      max_cycle_len = max(max_cycle_len, cycle_len(i));
+      m = max(m, cycle_len(i));
     }
-    printf("%d %d %d\n", I, J, max_cycle_len);
+    printf("%d %d %d\n", I, J, m);
   }
 
   return 0;
