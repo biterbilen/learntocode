d6325c5172f014e6ee7ba570a9805973199c9a0d
iamslash <iamslash@gmail.com>
2016-10-20 18:18:21 +0900

    php ing...

--- programming-challenges.com/The3n+1problem/a.php
+++ programming-challenges.com/The3n+1problem/a.php
@@ -1,1 +1,10 @@
-echo('<p>Hello World</p>');
+// Copyright (C) 2016 by iamslash
+
+// > php a.php
+
+while (fscanf(STDIN, '%d %d', $number1, $number2) === 2){
+    // Solve the test case and output the answer
+  var r = 0
+  fprintf(STDOUT, "%d\n", r);
+}
+
