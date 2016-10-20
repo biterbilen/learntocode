f924ba1831031810a73bd8bf1ca9d3880fba8601
iamslash <iamslash@gmail.com>
2016-10-20 13:07:36 +0900

    a.py ing...

--- programming-challenges.com/The3n+1problem/a.py
+++ programming-challenges.com/The3n+1problem/a.py
@@ -1,8 +1,29 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2016 by iamslash
+# > python3 a.py
 
-def main():
-    pass
+import sys
+
+CACHE = []
+I = 0
+J = 0
+
+def cycle_len(n):
+    
+    
+    return 0
 
 if __name__ == "__main__":
-    main()
+    for line in sys.stdin:
+        CACHE = [-1] * 1000000
+        
+        line = line.strip()
+        l = line.split(' ')
+        I = int(l[0])
+        J = int(l[1])
+
+        r = 0
+        for n in range(I, J+1):
+            r = max(r, cycle_len(n))
+            
+        print('{} {} {}'.format(I, J, r))
