7453b40d1b51185caec1739e7abe3f2232e04de6
iamslash <iamslash@gmail.com>
2016-11-22 18:11:15 +0900

    Topcompetitors fixed

--- hackerrank/sql/TopCompetitors.sql
+++ hackerrank/sql/TopCompetitors.sql
@@ -1,14 +1,13 @@
-/*
-Enter your query here.
-*/
-
-SELECT DISTINCT H.hacker_id, H.name /*, C.challenge_id, D.score, S.score */
-FROM HACKERS H 
-INNER JOIN Submissions S
+SELECT S.hacker_id, H.name
+FROM Submissions S 
 INNER JOIN Challenges C
+INNER JOIN Hackers H
 INNER JOIN Difficulty D
-ON H.hacker_id = S.hacker_id
-AND S.challenge_id = C.challenge_id
+ON S.challenge_id = C.challenge_id
+AND S.hacker_id = H.hacker_id
 AND C.difficulty_level = D.difficulty_level
-WHERE S.score >= D.score
-ORDER BY H.hacker_id ASC;
+AND D.score = S.score
+GROUP BY S.hacker_id, H.name
+HAVING COUNT(C.challenge_id) > 1
+ORDER BY COUNT(S.score) DESC, S.hacker_id ASC;
+
