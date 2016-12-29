4d922b00b223c85ba57e695e1c2e51553e832605
iamslash <iamslash@gmail.com>
2016-12-29 12:27:49 +0900

    ITES ing...

--- algospot.com/BRACKETS2/a.cpp
+++ algospot.com/BRACKETS2/a.cpp
@@ -1,76 +1,47 @@
 // Copyright (C) 2016 by iamslash
-// https://algospot.com/judge/problem/read/CHRISTMAS
+// https://algospot.com/judge/problem/read/BRACKETS2
 
 #include <cstdio>
 #include <string>
-#include <vector>
-#include <iostream>
-#include <cstring>
-#include <algorithm>
-// #include <set>
-#include <cmath>
-#include <queue>
-#include <cassert>
-#include <cstdint>
 #include <stack>
 
-int N, K;
+bool Solve(const std::string& s) {
 
-void print_vector(const std::vector<int>& v) {
-  printf("<-- ");
-  for (int i = 0; i < v.size(); ++i) {
-    printf("%d ", v[i]);
-  }
-  printf("-->\n");
-}
+  std::stack<char> bs;
 
-bool is_closed(const std::string& brackets) {
-  // printf("> %lu \n", brackets.size());
-  std::stack<char> bstack;
-  for (int i = 0; i < brackets.size(); ++i) {
-    char c = brackets[i];
+  for (const char& c : s) {
     if (c == '(' || c == '{' || c == '[') {
-      bstack.push(c);
+      bs.push(c);
     } else if (c == ')') {
-      if (bstack.size() == 0)
-        return false;
-      else if (bstack.top() == '(')
-        bstack.pop();
+      if (bs.top() == '(') {
+        bs.pop();
+      } else { return false; }      
     } else if (c == '}') {
-      if (bstack.size() == 0)
-        return false;
-      else if (bstack.top() == '{')
-        bstack.pop();
+      if (bs.top() == '{') {
+        bs.pop();
+      } else { return false; }      
     } else if (c == ']') {
-      if (bstack.size() == 0)
-        return false;
-      else if (bstack.top() == '[')
-        bstack.pop();
-    } else {
-      break;
+      if (bs.top() == '[') {
+        bs.pop();
+      } else { return false; }      
     }
   }
-  if (bstack.size() > 0)
+
+  if (bs.size() > 0)
     return false;
+
   return true;
 }
 
 int main() {
-  int T;  // number of T
+
+  int T;
   scanf("%d", &T);
-  //
   for (int t = 0; t < T; ++t) {
-    char buf[10000] = {0, };
-    //
+    char buf[10001] = {0,};
     scanf("%s", buf);
-    //
-    std::string brackets = buf;
-    //
-    if (is_closed(brackets))
-      printf("YES");
-    else
-      printf("NO");
-    printf("\n");
+    printf("%s\n", Solve(std::string(buf)) ? "YES" : "NO");
   }
+
   return 0;
 }
--- algospot.com/ITES/a.cpp
+++ algospot.com/ITES/a.cpp
@@ -1,9 +1,37 @@
+// Copyright (C) 2016 by iamslash
+// https://algospot.com/judge/problem/read/ITES
+
 #include <cstdio>
 #include <queue>
 
+int K, N;
+
+class ASig {
+  
+ public:
+  int Next() {
+    return 0;
+  }
+};
+
+int Solve(int k, int n) {
+  ASig signal;
+
+  std::queue<int> q;
+
+  //
+
+  return 0;
+}
+
 int main() {
 
-
+  int T;
+  scanf("%d", &T);
+  for (int t = 0; t < T; ++t) {
+    scanf("%d %d", &K, &N);
+    printf("%d", Solve(K, N));
+  }
   
   return 0;
 }
