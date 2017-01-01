65d938242d81376e09f26fae79f8e0d4b7f41c1a
iamslash <iamslash@gmail.com>
2017-01-01 21:17:06 +0900

    FENCE ing...

--- algospot.com/FENCE/a.cpp
+++ algospot.com/FENCE/a.cpp
@@ -1,6 +1,32 @@
+// Copyright (C) 2017 by iamslash
+// https://algospot.com/judge/problem/read/FENCE
+
 #include <cstdio>
+#include <stack>
+#include <vector>
+
+int N;
+
+int Solve(const std::vector<int>& fence) {
+  
+  
+  
+  return 0;
+}
 
 int main() {
+  int T;
+  scanf("%d", &T);
+  for (int t = 0; t < T; ++t) {
+    scanf("%d", &N);
+    std::vector<int> fence(N, 0);
+    for (int i = 0; i < N; ++i) {
+      scanf("%d", &fence[i]);
+    }
+    fence.insert(fence.begin(), -1);
+    fence.push_back(-1);
+    printf("%d\n", Solve(fence));
+  }
 
   return 0;
 }
