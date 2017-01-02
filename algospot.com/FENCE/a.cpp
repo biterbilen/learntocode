30e0f65841188d27e8337d726d33e1338f6bca5b
iamslash <iamslash@gmail.com>
2017-01-02 21:40:43 +0900

    FENCE finished

--- algospot.com/FENCE/a.cpp
+++ algospot.com/FENCE/a.cpp
@@ -8,10 +8,31 @@
 int N;
 
 int Solve(const std::vector<int>& fence) {
-  
-  
-  
-  return 0;
+  int r = 0;
+  // stack for height
+  std::stack<int> stck;
+  stck.push(0);
+
+  for (int i = 1; i < fence.size(); ++i) {
+    while (stck.empty() == false &&
+           fence[stck.top()] >= fence[i]) {
+      int idx = stck.top();
+      stck.pop();
+      // printf(" %d %d\n", i, idx);
+
+      // calculate area
+      int left = stck.top();
+      int right = i;
+      int area = (right - left - 1) * fence[idx];
+      r = std::max(r, area);
+      // printf("  %d %d %d\n", i, idx, r);
+    }
+    //
+    stck.push(i);
+  }
+
+  // printf("  %d\n", r);
+  return r;
 }
 
 int main() {
@@ -24,7 +45,7 @@ int main() {
       scanf("%d", &fence[i]);
     }
     fence.insert(fence.begin(), -1);
-    fence.push_back(-1);
+    fence.push_back(0);
     printf("%d\n", Solve(fence));
   }
 
