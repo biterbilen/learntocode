f2e3ebc73810ea00d2876cbdc91ffea69c4b7465
iamslash <iamslash@gmail.com>
2017-01-27 16:31:03 +0900

    LAN, TPATH kick off

---
+++ algospot.com/LAN/a.cpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2017 by iamslash
+// https://algospot.com/judge/problem/read/LAN
+
+#include <cstdio>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+#define MAX_V 500
+#define MAX_I 987654321
