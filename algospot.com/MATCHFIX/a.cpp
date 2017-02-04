4b4a61f743fecfe9fee3d8cc557f576609cb0df2
iamslash <iamslash@gmail.com>
2017-02-04 21:33:01 +0900

    MATCHFIX, PROJECTS finished but failed

--- algospot.com/MATCHFIX/a.cpp
+++ algospot.com/MATCHFIX/a.cpp
@@ -1,24 +1,22 @@
 // Copyright (C) 2017 by iamslash
-// https://algospot.com/judge/problem/read/TIMETRIP
+// https://algospot.com/judge/problem/read/MATCHFIX
 
 #include <cstdio>
 #include <string>
 #include <algorithm>
 #include <vector>
 #include <queue>
-#include <numeric>
 
-#define MAX_V 300
+#define MAX_V 200
 #define MAX_I 987654321
 
 int N, M;
-int profit[100];
-int cost[100];
+int wins[20];
+int match[200][2];
 
 int V;
 int capacity[MAX_V][MAX_V];
 int flow[MAX_V][MAX_V];
-int adj[MAX_V][MAX_V];
 
 int FordFulkerson(int source, int sink) {
   int r = 0;
@@ -57,27 +55,30 @@ int FordFulkerson(int source, int sink) {
   return r;
 }
 
-int MaxProfit() {
-  const int source = 0;
-  const int sink = 1;
-
-  for (int i = 0; i < N; ++i) {
-    capacity[source][2 + i] = profit[i];
-  }
+bool CanWinWith(int total_win) {
+  // 0 has the max wins ???
+  if (*std::max_element(wins+1, wins+N) >= total_win)
+    return false;
   for (int i = 0; i < M; ++i) {
-    capacity[2 + N + i][sink] = cost[i];
-  }
-  for (int i = 0; i < N; ++i) {
-    for (int j = 0; j < M; ++j) {
-      if (adj[i][j]) {
-        capacity[2 + i][2 + N + j] = MAX_I;
-      }
+    capacity[0][2 + i] = 1;
+    for (int j = 0; j < 2; ++j) {
+      capacity[2 + i][2 + M + match[i][j]] = 1;
+    }
+    for (int i = 0; i < N; ++i) {
+      int maxwin = (i == 0 ? total_win : total_win - 1);
+      capacity[2 + M + i][1] = maxwin - wins[i];
     }
   }
-  int M = std::accumulate(profit, profit + N, 0);
-  int C = FordFulkerson(source, sink);
+  return FordFulkerson(0, 1) == M;
+}
 
-  return M - C;
+int Solve(int from_win) {
+  for (int i = 0; i < M; ++i) {
+    int total_win = from_win + i;
+    if (CanWinWith(total_win))
+      return total_win;
+  }
+  return -1;
 }
 
 int main() {
@@ -85,28 +86,24 @@ int main() {
   scanf("%d", &T);
   for (int t = 0; t < T; ++t) {
     scanf("%d %d", &N, &M);
-    V = 2 + N + M;
     for (int i = 0; i < N; ++i) {
-      scanf("%d", &profit[i]);
+      scanf("%d", &wins[i]);
     }
     for (int i = 0; i < M; ++i) {
-      scanf("%d", &cost[i]);
+      scanf("%d %d", &match[i][0], &match[i][1]);
     }
 
-    for (int i = 0; i < N; ++i) {
-      for (int j = 0; j < M; ++j) {
-        scanf("%d", &adj[i][j]);
-      }
-    }
-
+    // init variables
     for (int i = 0; i < MAX_V; ++i) {
       for (int j = 0; j < MAX_V; ++j) {
         capacity[i][j] = 0;
         flow[i][j] = 0;
       }
     }
+    V = 2 + M + N;
 
-    printf("%d\n", MaxProfit());
+    printf("%d\n", Solve(wins[0]));
   }
+
   return 0;
 }
