5f733f5eb7a1a565601d68d58358fe2c9651b6e9
iamslash <iamslash@gmail.com>
2017-02-18 23:44:10 +0900

    NQUEEN, TRAPCARD added

---
+++ algospot.com/NQUEEN/a.cpp
@@ -0,0 +1,34 @@
+// Copyright (C) 2017 by iamslash
+// https://algospot.com/judge/problem/read/NQUEENS
+
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <cstring>
+
+int N;
+int Q[12];
+
+bool Nqueen(int y, int x) {
+  
+}
+
+int Solve() {
+  int r = 0;
+
+  for (int y = 0; y < N; ++y) {
+    
+  }
+  
+  return r;
+}
+
+int main() {
+  int T;
+  scanf("%d", &T);
+  for (int t = 0; t < T; ++t) {
+    memset(Q, -1, sizeof(Q));
+    scanf("%d", &N);
+    printf("%d\n", Solve());
+  }
+}
