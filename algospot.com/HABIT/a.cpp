9ced1bb54a28c6b2e9116668e4bb859c15a9f5ff
iamslash <iamslash@gmail.com>
2017-02-19 10:41:23 +0900

    HABBIT accepted

--- algospot.com/HABIT/a.cpp
+++ algospot.com/HABIT/a.cpp
@@ -7,6 +7,12 @@
 
 int K;
 
+void PrintSuffixArray(const std::string& s, const std::vector<int>& sa) {
+  for (int i = 0; i < sa.size(); ++i) {
+    printf("  [%2d] %-10s\n", i, s.c_str() + sa[i]);
+  }
+}
+
 std::vector<int> GetSuffixArray(const std::string& s) {
   std::vector<int> r(s.size());
   for (int i = 0; i < s.size(); ++i) {
@@ -20,26 +26,25 @@ std::vector<int> GetSuffixArray(const std::string& s) {
   return r;
 }
 
-int GetLcpCnt(const std::string& s, const std::vector<int>& sa, int i) {
+int GetLcpCnt(const std::string& s, const std::vector<int>& sa, int i, int j) {
   int r = 0;
-  int j = i + K - 1;
-
   while (i < s.size() && j < s.size() && s[i] == s[j]) {
-    r++;
-    i++;
-    j++;
+    ++r;
+    ++i;
+    ++j;
   }
-
+  // printf("    i: %2d j: %2d r: %2d\n", i, j, r);
   return r;
 }
 
-int Solve(std::string s) {
+int Solve(const std::string& s) {
   int r = 0;
 
   std::vector<int> sa = GetSuffixArray(s);
+  // PrintSuffixArray(s, sa);
 
-  for (int i = 0; i + K < sa.size(); ++i) {
-    r = std::max(r, GetLcpCnt(s, sa, i));
+  for (int i = 0; i + K <= sa.size(); ++i) {
+    r = std::max(r, GetLcpCnt(s, sa, sa[i], sa[i + K - 1]));
   }
 
   return r;
@@ -50,7 +55,7 @@ int main() {
   scanf("%d", &T);
   for (int t = 0; t < T; ++t) {
     scanf("%d", &K);
-    char buf[4000];
+    char buf[5000];
     scanf("%s", buf);
 
     printf("%d\n", Solve(buf));
