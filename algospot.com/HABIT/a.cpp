b32a1057968d6dfee67e55401e427920a94e30fd
iamslash <iamslash@gmail.com>
2017-03-12 21:02:55 +0900

    HABIT finished

--- algospot.com/HABIT/a.cpp
+++ algospot.com/HABIT/a.cpp
@@ -1,53 +1,35 @@
 // Copyright (C) 2017 by iamslash
 
-#include <cstring>
+#include <cstdio>
 #include <vector>
-#include <algorithm>
 #include <string>
+#include <algorithm>
+#include <cstring>
 
 int K;
 
-void PrintSuffixArray(const std::string& s, const std::vector<int>& sa) {
-  for (int i = 0; i < sa.size(); ++i) {
-    printf("  [%2d] %-10s\n", i, s.c_str() + sa[i]);
-  }
-}
-
-std::vector<int> GetSuffixArray(const std::string& s) {
+std::vector<int> get_sa(const std::string& s) {
   std::vector<int> r(s.size());
-  for (int i = 0; i < s.size(); ++i) {
+  for (int i = 0; i < s.size(); ++i)
     r[i] = i;
-  }
-  // std::sort(r.begin(), r.end(), Comp(s));
   std::sort(r.begin(), r.end(),
-            [s](int i, int j) -> bool {
-              return strcmp(s.c_str()+i, s.c_str()+j) < 0;
+            [s](int i, int j)->bool {
+              return strcmp(s.substr(i).c_str(),
+                            s.substr(j).c_str()) < 0;
             });
   return r;
 }
 
-int GetLcpCnt(const std::string& s, const std::vector<int>& sa, int i, int j) {
-  int r = 0;
-  while (i < s.size() && j < s.size() && s[i] == s[j]) {
-    ++r;
+int get_common_prefix(const std::string& a, const std::string& b) {
+  int i = 0, j = 0, k = 0;
+
+  while (i < a.size() && j < b.size() && a[i] == b[j]) {
     ++i;
     ++j;
-  }
-  // printf("    i: %2d j: %2d r: %2d\n", i, j, r);
-  return r;
-}
-
-int Solve(const std::string& s) {
-  int r = 0;
-
-  std::vector<int> sa = GetSuffixArray(s);
-  // PrintSuffixArray(s, sa);
-
-  for (int i = 0; i + K <= sa.size(); ++i) {
-    r = std::max(r, GetLcpCnt(s, sa, sa[i], sa[i + K - 1]));
+    ++k;
   }
 
-  return r;
+  return k;
 }
 
 int main() {
@@ -55,9 +37,25 @@ int main() {
   scanf("%d", &T);
   for (int t = 0; t < T; ++t) {
     scanf("%d", &K);
-    char buf[5000];
+    char buf[4096];
     scanf("%s", buf);
+    std::string s = buf;
+    std::vector<int> sa = get_sa(s);
 
-    printf("%d\n", Solve(buf));
+    int r = 0;
+    for (int i = 0; i + K - 1 < s.size(); ++i) {
+      std::string a = s.substr(sa[i]);
+      std::string b = s.substr(sa[i+K-1]);
+      r = std::max(r, get_common_prefix(a, b));
+    }
+
+    printf("%d\n", r);
   }
+
+  return 0;
 }
+
+
+
+
+
