f4592dcd8a482d4cfc958387001df5fc57a2d620
iamslash <iamslash@gmail.com>
2017-03-25 12:40:26 +0900

    TRAVERSAL finished

--- algospot.com/TRAVERSAL/a.cpp
+++ algospot.com/TRAVERSAL/a.cpp
@@ -9,12 +9,25 @@
 int N;
 
 // make slice vector from a to b of v
-std::vector<int> slice(const std::vector<int>& v,
-                       const std::vector<int>::const_iterator& a,
-                       const std::vector<int>::const_iterator& b) {
-  if (a == v.end())
+std::vector<int> slice(const std::vector<int>& v, int a, int b) {
+  if (a >= v.size())
     return std::vector<int>();
-  return std::vector<int>(a, b);
+
+  // printf("  ");
+  // for (const int& i : v) {
+  //   printf("%d ", i);
+  // }
+  // printf(" : %d, %d\n", a, b);
+
+  std::vector<int> r = std::vector<int>(v.begin() + a, v.begin() + b);
+
+  // printf("    ");
+  // for (const int& i : r) {
+  //   printf("%d ", i);
+  // }
+  // printf("\n");
+
+  return r;
 }
 
 void Solve(const std::vector<int>& p_order, const std::vector<int>& i_order) {
@@ -27,14 +40,12 @@ void Solve(const std::vector<int>& p_order, const std::vector<int>& i_order) {
   auto it = std::find(i_order.begin(), i_order.end(), root);
   int cnt = it - i_order.begin();
 
-  std::vector<int> p_order_l = slice(p_order, p_order.begin() + 1,
-                                     p_order.begin() + cnt);
-  std::vector<int> p_order_r = slice(p_order, p_order.begin() + cnt,
-                                     p_order.end());
-  std::vector<int> i_order_l = slice(i_order, i_order.begin(),
-                                     i_order.begin() + cnt);
-  std::vector<int> i_order_r = slice(i_order, i_order.begin() + cnt,
-                                     i_order.end());
+  // 27, 3
+  // printf("  %d %d\n", root, cnt);
+  std::vector<int> p_order_l = slice(p_order, 1, cnt + 1);
+  std::vector<int> p_order_r = slice(p_order, cnt + 1, p_order.size());
+  std::vector<int> i_order_l = slice(i_order, 0, cnt);
+  std::vector<int> i_order_r = slice(i_order, cnt + 1, i_order.size());
 
   Solve(p_order_l, i_order_l);
   Solve(p_order_r, i_order_r);
@@ -53,6 +64,7 @@ int main() {
       for (int i = 0; i < N; ++i)
           scanf("%d", &i_order[i]);
       Solve(p_order, i_order);
+      printf("\n");
   }
   // std::vector<int> a(10);
   // std::vector<int> b = slice(a, 0, 1);
