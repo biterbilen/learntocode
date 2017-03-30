3cc0f435505519bd863df689ff2effe4b22de443
iamslash <iamslash@gmail.com>
2017-03-30 22:34:08 +0900

    Spreadsheet wrong answer

--- codeforces/Spreadsheets/a.cpp
+++ codeforces/Spreadsheets/a.cpp
@@ -3,94 +3,99 @@
 
 #include <cstdio>
 #include <cstdint>
+#include <cstdlib>
 #include <string>
 #include <regex>
 
 int get_num(const std::string& s) {
   int r = 0;
   int div = 1;
-  for (int i = 0; i < s.size(); ++i, div *= 26) {
-    int n = atoi(&s[i]);
+  for (int i = s.size() - 1; i >= 0; --i, div *= 26) {
+    int n = s[i] - 'A' + 1;
+    // printf("  %c %d\n", s[i], n);
     r += (n * div);
   }
   return r;
 }
 
 char get_cap_chr(int n) {
-  return 'A' + n;
+  return 'A' + n - 1;
 }
 
 std::string get_str(int n) {
   std::string r = "";
   int div = n;
   int mod;
-  do {
+  while (true) {
     n = div;
-    div = n / 26;
-    mod = n % 26;   
-    // printf("  %d\n", div);
-    if (div == 0) {
-      r.insert(0, 1, get_cap_chr(mod));
-    } else {
-      r.insert(0, 1, get_cap_chr(div));
-    }
+    div = n / 26; // 2
+    mod = n % 26; // 3
+    r.insert(0, 1, get_cap_chr(mod));
     // printf("%s\n", r);
-  } while (div > 0);
+    if (div == 0)
+      break;
+  }
   return r;
 }
 
+// BC23
 std::string get_numeration_1(const std::string row,
                              const std::string col) {
-  std::string r;
-  // R23C234
-  return r;
+  char buf[128];
+  // R23C55
+  snprintf(buf, sizeof(buf), "R%sC%d", row.c_str(), get_num(col));
+  return buf;
 }
 
+// R23C55
 std::string get_numeration_2(const std::string row,
                              const std::string col) {
-
-  std::string r;
-  // B33
-  return r;
+  char buf[128];
+  // BC23
+  snprintf(buf, sizeof(buf), "%s%s",
+           get_str(atoi(col.c_str())).c_str(),
+           row.c_str());
+  return buf;
 }
 
 int main() {
-  // std::regex re0("[A-Z]+[0-9]+");
-  // std::regex renum("[0-9]+");
-  // std::regex restr("[A-Z]+");
-  // int N;
-  // scanf("%d", &N);
-  // for (int i = 0; i < N; ++i) {
-  //   char buf[128];
-  //   scanf("%s", buf);
-  //   std::string msg = buf;
+  std::regex re0("[A-Z]+[0-9]+");
+  std::regex renum("[0-9]+");
+  std::regex restr("[A-Z]+");
+  int N;
+  scanf("%d", &N);
+  for (int i = 0; i < N; ++i) {
+    char buf[128];
+    scanf("%s", buf);
+    std::string msg = buf;
 
-  //   std::smatch mresult;
-  //   std::string row;
-  //   std::string col;
+    std::smatch mresult;
+    std::string row;
+    std::string col;
 
-  //   if (std::regex_match(msg, re0)) {
-  //     std::regex_search(msg, mresult, restr);
-  //     row = mresult[0];
-  //     std::regex_search(msg, mresult, renum);
-  //     col = mresult[0];
-  //   } else {
-  //     while (std::regex_search(msg, mresult, renum)) {
-  //       if (row.size() == 0)
-  //         row = mresult[0];
-  //       else
-  //         col = mresult[0];
-  //       msg = mresult.suffix().str();
-  //     }
-  //   }
-  //   // printf("%s %s\n", row.c_str(), col.c_str());
-  // }
+    std::string r;
+    if (std::regex_match(msg, re0)) {
+      std::regex_search(msg, mresult, restr);
+      col = mresult[0];
+      std::regex_search(msg, mresult, renum);
+      row = mresult[0];
+      r = get_numeration_1(row, col);
+    } else {
+      while (std::regex_search(msg, mresult, renum)) {
+        if (row.size() == 0)
+          row = mresult[0];
+        else
+          col = mresult[0];
+        msg = mresult.suffix().str();
+      }
+      // printf("  %s %s\n", row.c_str(), col.c_str());
+      r = get_numeration_2(row, col);
+    }
+    printf("%s\n", r.c_str());
+  }
 
-  // for (int i = 0; i < 26; ++i)
-  //   printf("%c ", get_cap_chr(i));
-  // printf("\n");
-  printf("%s\n", get_str(54));
-  // printf("%d\n", get_num("A"));
-  
+  // printf("%s\n", get_str(55).c_str());
+  // printf("%d\n", get_num("BC"));
+
   return 0;
 }
