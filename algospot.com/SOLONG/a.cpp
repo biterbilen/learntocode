358289d6ec55dee24c685b0740a959b3ac7345c7
David Sun <iamslash@gmail.com>
2017-04-02 22:33:02 +0900

    SOLONG ing...

--- algospot.com/SOLONG/a.cpp
+++ algospot.com/SOLONG/a.cpp
@@ -1,6 +1,56 @@
 // Copyright (C) 2016 by iamslash
 // https://algospot.com/judge/problem/read/SOLONG
 
+#include <cstdio>
+#include <vector>
+#include <map>
+#include <string>
+
+int N, M;
+std::vector<std::pair<int, int>> DICT;
+std::vector<std::string> DICTWORDS;
+std::vector<int> PRIORITIES;
+std::vector<std::string> WORDS;
+
+void print_v_string(const std::vector<std::string>& v) {
+  for (auto x : v)
+    printf("%s ", x.c_str());
+  printf("\n");
+}
+
+class TrieNode {
+};
+
+int solve() {
+  
+  
+  return 0;
+}
+
 int main() {
+  int T;
+  scanf("%d", &T);
+  for (int t = 0; t < T; ++t) {
+    scanf("%d %d", &N, &M);
+    DICT.clear(); DICT.resize(N);
+    DICTWORDS.clear(); DICTWORDS.resize(N);
+    PRIORITIES.clear(); PRIORITIES.resize(N);
+    WORDS.clear(); WORDS.resize(M);
+    for (int i = 0; i < N; ++i) {
+      char buf[16];
+      int priority;
+      scanf("%s %d", buf, &priority);
+      DICTWORDS[i] = buf;
+      PRIORITIES[i] = priority;
+      DICT[i] = std::make_pair(i, priority);
+    }
+    for (int i = 0; i < M; ++i) {
+      char buf[16];
+      scanf("%s", buf);
+      WORDS[i] = buf;
+    }
+    print_v_string(WORDS);
+    // printf("%d\n", solve());
+  }
   return 0;
 }
