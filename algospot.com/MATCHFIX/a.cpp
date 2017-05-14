78e96ddac672ef5c2077c4b81a7081332ce46287
iamslash <iamslash@gmail.com>
2017-05-14 10:14:24 +0900

    MATCHFIX again

--- algospot.com/MATCHFIX/a.cpp
+++ algospot.com/MATCHFIX/a.cpp
@@ -18,70 +18,13 @@ int V;
 int capacity[MAX_V][MAX_V];
 int flow[MAX_V][MAX_V];
 
-int FordFulkerson(int source, int sink) {
-  int r = 0;
-
-  while (true) {
-    std::vector<int> parent(V, -1);
-    std::queue<int> q;
-    parent[source] = source;
-    q.push(source);
-    while (!q.empty() && parent[sink] == -1) {
-      int here = q.front();
-      q.pop();
-      for (int there = 0; there < V; ++there) {
-        if (capacity[here][there] - flow[here][there] &&
-            parent[there] == -1) {
-          q.push(there);
-          parent[there] = here;
-        }
-      }
-    }
-    // terminate if there is no augmenting path
-    if (parent[sink] == -1)
-      break;
-
-    int amount = MAX_I;
-    for (int p = sink; p != source; p = parent[p]) {
-      amount = std::min(capacity[parent[p]][p] - flow[parent[p]][p], amount);
-    }
-    for (int p = sink; p != source; p = parent[p]) {
-      flow[parent[p]][p] += amount;
-      flow[p][parent[p]] -= amount;
-    }
-    r += amount;
-  }
-
-  return r;
+int ford_fulkerson(int source, int sink) {
 }
 
-bool CanWinWith(int total_win) {
-  // 0 has the max wins ???
-  if (*std::max_element(wins+1, wins+N) >= total_win)
-    return false;
-  for (int i = 0; i < M; ++i) {
-    // from source to match
-    capacity[0][2 + i] = 1;
-    // from match to person
-    for (int j = 0; j < 2; ++j) {
-      capacity[2 + i][2 + M + match[i][j]] = 1;
-    }
-    // from person to sink
-    for (int i = 0; i < N; ++i) {
-      int maxwin = (i == 0 ? total_win : total_win - 1);
-      capacity[2 + M + i][1] = maxwin - wins[i];
-    }
-  }
-  return FordFulkerson(0, 1) == M;
+bool can_win_with(int total_win) {
 }
 
-int Solve(int from_win) {
-  for (int i = 0; i < M; ++i) {
-    int total_win = from_win + i;
-    if (CanWinWith(total_win))
-      return total_win;
-  }
-  return -1;
+int solve(int from_win) {
 }
 
 int main() {
@@ -104,8 +47,7 @@ int main() {
       }
     }
     V = 2 + M + N;
-
-    printf("%d\n", Solve(wins[0]));
+    printf("%d\n", solve(wins[0]));
   }
 
   return 0;
