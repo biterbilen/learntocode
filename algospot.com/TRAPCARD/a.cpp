a3db59910ca62d8573a2273920c36c7e7df40d3d
iamslash <iamslash@gmail.com>
2017-05-28 12:06:16 +0900

    TRAPCARD again

--- algospot.com/TRAPCARD/a.cpp
+++ algospot.com/TRAPCARD/a.cpp
@@ -1,5 +1,4 @@
 // Copyright (C) 2017 by iamslash
-// https://algospot.com/judge/problem/read/TRAPCARD
 
 #include <cstdio>
 #include <vector>
@@ -13,6 +12,7 @@ const int dx[4] = {1, -1, 0, 0};
 const int dy[4] = {0, 0, 1, -1};
 std::vector<std::vector<int> > id;
 
+// trap for id
 std::vector<bool> a_chosen;
 std::vector<bool> b_chosen;
 
@@ -20,11 +20,10 @@ int L, R;
 std::vector<int> a_match;
 std::vector<int> b_match;
 std::vector<bool> visited;
-// adj[i][j] = id가 i인 B그리드와 id가 j인 W그리드의 capacity
 int adj[400][400];
 
 bool in_range(int y, int x) {
-  if ( y < 0 || y >= H || x < 0 || x >= W)
+  if ( y < 0 || y >= H || x < 0 || x >> W)
     return false;
   return true;
 }
@@ -34,7 +33,6 @@ void build_adj() {
   id.resize(H, std::vector<int>(W, -1));
   L = 0;
   R = 0;
-  // BOARD는 B, W그리드들이 서로 인접하여 구성되어 있다.
   for (int y = 0; y < H; ++y) {
     for (int x = 0; x < W; ++x) {
       if (BOARD[y][x] != '#') {
@@ -46,17 +44,17 @@ void build_adj() {
       }
     }
   }
-  memset(adj, 0, sizeof(adj));
-  // (x1, y1)을 중심으로 사방을 (x2, y2)로 표현하자.
+  for (int i = 0; i < 400; ++i)
+    for (int j = 0; j < 400; ++j)
+      adj[i][j] = 0;
   for (int y1 = 0; y1 < H; ++y1) {
     for (int x1 = 0; x1 < W; ++x1) {
       if ((y1 + x1) % 2 == 0 && BOARD[y1][x1] != '#') {
         for (int k = 0; k < 4; ++k) {
           int y2 = y1 + dy[k];
           int x2 = x1 + dx[k];
-          if (in_range(y2, x2) && BOARD[y2][x2] != '#') {
+          if (in_range(y2, x2) && BOARD[y2][x2] != '#')
             adj[id[y1][x1]][id[y2][x2]] = 1;
-          }
         }
       }
     }
@@ -83,11 +81,13 @@ int bipartite_match() {
   a_match = std::vector<int>(L, -1);
   b_match = std::vector<int>(R, -1);
   int r = 0;
+
   for (int start = 0; start < L; ++start) {
     visited = std::vector<bool>(L, false);
     if (dfs(start))
       ++r;
   }
+
   return r;
 }
 
@@ -104,14 +104,15 @@ void calc_max_independent_sets() {
     bool changed = false;
     for (int i = 0; i < L; ++i) {
       for (int j = 0; j < R; ++j) {
-        if (a_chosen[i] && b_chosen[j] && adj[i][j]) {
-          assert(a_match[i] != -1);
+        if (a_chosen[i] && b_chosen[j] && adj[i][j] == 1) {
+          //assert(a_match[i] != -1);
           a_chosen[i] = false;
           b_chosen[a_match[i]] = true;
           changed = true;
         }
       }
     }
+
     if (!changed)
       break;
   }
@@ -128,7 +129,6 @@ void print_result() {
       ++r;
   }
   printf("%d\n", r);
-
   for (int y = 0; y < H; ++y) {
     for (int x = 0; x < W; ++x) {
       if (BOARD[y][x] == '#') {
@@ -152,7 +152,7 @@ int main() {
     scanf("%d %d", &H, &W);
     BOARD.clear();
     for (int i = 0; i < H; ++i) {
-      char buf[24] = {0, };
+      char buf[24];
       scanf("%s", buf);
       BOARD.push_back(buf);
     }
