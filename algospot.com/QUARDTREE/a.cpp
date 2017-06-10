97a95f55a00fd3f363b4219e0e00795a6c0fc1c7
iamslash <iamslash@gmail.com>
2017-06-10 10:19:38 +0900

    QUADTREE finished

--- algospot.com/QUARDTREE/a.cpp
+++ algospot.com/QUARDTREE/a.cpp
@@ -1,83 +1,35 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
+// Copyright (C) 2017 by iamslash
+// https://algospot.com/judge/problem/read/QUADTREE
 
+#include <cstdio>
 #include <string>
 
-using namespace std;
+std::string reverse(std::string::iterator & it) {
+  char c = *it;
+  it++;
 
-#define MAX 16
+  // base condition
+  if (c == 'w' || c == 'b')
+    return std::string(1, c);
 
-void dump(char P[][MAX])
-{
-    for (int i=0; i<MAX; ++i)
-    {
-        for (int j=0; j<MAX; ++j)
-            printf("%c ", P[i][j]);
-        printf("\n");
-    }
-}
-
-void decode(char P[][MAX], char M[], int * p_idx, int x, int y, int size)
-{
-    char m = M[*p_idx];
-
-    /* printf("-----> %c\n", m); */
-
-    (*p_idx)++;
-    
-    // base condition
-    if (m == 'w' || m == 'b')
-    {
-        for (int i=y; i<y+size; ++i)
-            for (int j=x; j<x+size; ++j)
-                P[i][j] = m;
-        return;
-    }
-
-    // recursion
-    decode(P, M, p_idx, x, y, size/2);
-    decode(P, M, p_idx, x + size/2, y, size/2);
-    decode(P, M, p_idx, x, y + size/2, size/2);
-    decode(P, M, p_idx, x + size/2, y + size/2, size/2);
-}
-
-string reverse(string::iterator & it)
-{
-    char m = *it;
-    it++;
-
-    // base condition
-    if (m == 'w' || m == 'b')
-        return string(1, m);
-
-    // recursion
-    string lt = reverse(it);
-    string rt = reverse(it);
-    string lb = reverse(it);
-    string rb = reverse(it);
-
-    return string("x") + lb + rb + lt + rt;
-    
+  // recursion
+  std::string lt = reverse(it);
+  std::string rt = reverse(it);
+  std::string lb = reverse(it);
+  std::string rb = reverse(it);
+  return "x" + lb + rb + lt + rt;
 }
 
 int main() {
-    
-    int C; // number of cases
-
-    scanf("%d", &C);
-    
-    for(int c=0; c<C; ++c)
-    {
-        char M[1000]; // coded string
-
-        scanf("%s", M);
-
-        string R = M;
-        string::iterator it_R = R.begin();
-
-        printf("%s\n", reverse(it_R).c_str());
-    }
-  
-    return 0;
+  int T;
+  scanf("%d", &T);
+  for (int t = 0; t < T; ++t) {
+    char buf[1024];
+    scanf("%s", buf);
+    std::string s = buf;
+    std::string::iterator it = s.begin();
+    printf("%s\n", reverse(it).c_str());
+  }
+  return 0;
 }
+
