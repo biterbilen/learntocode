782a77368bb83e839cb731ae5be4079cd9d0b59f
iamslash <iamslash@gmail.com>
2017-07-10 17:28:28 +0900

    OCR ing...

--- algospot.com/OCR/a.cpp
+++ algospot.com/OCR/a.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <cmath>
 #include <cassert>
+#include <cstring>
 
 int m, q, N;  // 원문에 출현할 수 있는 단어의 수, 처리해야할 문장의 수
 double B[501];  // 원문에 대하여 i가 문장의 처음에 출현할 확률
@@ -21,22 +22,28 @@ int word_to_idx(std::string s) {
   assert(0);
 }
 
-int solve(int ridx, int prev_match_idx) {
+int solve(int cur_recog_idx, int prev_match_idx) {
+  printf("  ");
+  for (int i = 0; i < cur_recog_idx; ++i)
+    printf(" ");
+  printf("%d %d\n", cur_recog_idx, prev_match_idx);
+  
   prev_match_idx++;
+
   // base condition
-  if (ridx == N)
+  if (cur_recog_idx == N)
     return 0;
-  double& r = CACHE[ridx][prev_match_idx];
+  double& r = CACHE[cur_recog_idx][prev_match_idx];  
   if (r != 1.0)
     return r;
   r = -1e200;
-  int& choose = CHOICE[ridx][prev_match_idx];
+  int& choose = CHOICE[cur_recog_idx][prev_match_idx];
   // recursion
   for (int cur_match_idx = 0; cur_match_idx < m; ++cur_match_idx) {
     double cand = (prev_match_idx == 0 ?
                    B[cur_match_idx] :
                    T[prev_match_idx][cur_match_idx]) +
-        M[cur_match_idx][R[ridx]] + solve(ridx + 1, cur_match_idx);
+        M[cur_match_idx][R[cur_recog_idx]] + solve(cur_recog_idx + 1, cur_match_idx);
     if (r < cand) {
       r = cand;
       choose = cur_match_idx;
@@ -46,17 +53,17 @@ int solve(int ridx, int prev_match_idx) {
 }
 
 // TODO(iamslash): prev_match_idx can be negative
-std::string reconstruct(int ridx, int prev_match_idx) {
+std::string reconstruct(int cur_recog_idx, int prev_match_idx) {
   prev_match_idx++;
-  int choose = CHOICE[ridx][prev_match_idx];
+  int choose = CHOICE[cur_recog_idx][prev_match_idx];
+  // printf("  %d %d %d\n", cur_recog_idx, prev_match_idx, choose);
   std::string r = WORDS[choose];
-  if (ridx < N - 1)
-    r += " " + reconstruct(ridx + 1, choose);
+  if (cur_recog_idx < N - 1)
+    r += " " + reconstruct(cur_recog_idx + 1, choose);
   return r;
 }
 
 int main() {
-  memset(CACHE, 1, sizeof(CACHE));
   scanf("%d %d", &m, &q);
   for (int i = 0; i < m; ++i) {
     char buf[1024];
@@ -65,21 +72,28 @@ int main() {
   }
   for (int i = 0; i < m; ++i) {
     scanf("%lf", &B[i]);
+    // B[i] = B[i] == 0.0 ? 0.0 : log(B[i]);
     B[i] = log(B[i]);
   }
   for (int i = 0; i < m; ++i) {
     for (int j = 0; j < m; ++j) {
       scanf("%lf", &T[i][j]);
+      // T[i][j] = T[i][j] == 0.0 ? 0.0 : log(T[i][j]);
       T[i][j] = log(T[i][j]);
     }
   }
   for (int i = 0; i < m; ++i) {
     for (int j = 0; j < m; ++j) {
       scanf("%lf", &M[i][j]);
+      // M[i][j] = M[i][j] == 0.0 ? 0.0 : log(M[i][j]);
       M[i][j] = log(M[i][j]);
     }
   }
   for (int i = 0; i < q; ++i) {
+    for (int i = 0; i < 102; ++i)
+      for (int j = 0; j < 502; ++j)
+        CACHE[i][j] = 1.0;
+
     scanf("%d", &N);
     for (int j = 0; j < N; ++j) {
       char buf[1024];
