559c16ba1fde8252d518da88b5b74eb262e5e821
iamslash <iamslash@gmail.com>
2017-07-25 14:04:28 +0900

    permutation without dups finished

--- crackingthecodinginterview/8.7_PermutationsWithoutDups/a.cpp
+++ crackingthecodinginterview/8.7_PermutationsWithoutDups/a.cpp
@@ -3,15 +3,33 @@
 // Permutations without Dups: Write a method to compute all
 // permutations of a string of unique characters.
 
+// design : backtracking
+// time complexity : O(n * n!)
+// space complexity : O(1)
+
 #include <cstdio>
 #include <string>
+#include <algorithm>
+#include <cstring>
 
-int N;
-std::string S;
+void print_indent(int indent) {
+  for (int i = 0; i < indent; ++i)
+    printf(" ");
+}
 
-void solve(std::string s) {
-  for (int i = 0; i < s.size(); ++i) {
-    
+void solve(int indent, char * s, int l, int r) {
+  print_indent(indent);
+  printf("%d %d\n", l , r);
+  // base condition
+  if (l == r - 1) {
+    print_indent(indent);
+    printf("%s\n", s);
+  } else {
+    for (int i = l; i < r; ++i) {
+      std::swap(*(s + l), *(s + i));
+      solve(indent + 1, s, l + 1, r);
+      std::swap(*(s + l), *(s + i));
+    }
   }
 }
 
@@ -21,8 +39,7 @@ int main() {
   for (int t = 0; t < T; ++t) {
     char buf[1024];
     scanf("%s", buf);
-    S = buf;
-    solve(S);
+    solve(0, buf, 0, strlen(buf));
   }
   return 0;
 }
