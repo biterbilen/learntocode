20b025fe7ad0a3e0a5227937551f31179167c0e4
iamslash <iamslash@gmail.com>
2017-07-25 11:53:35 +0900

    permutation without dups ing...

---
+++ crackingthecodinginterview/8.7_PermutationsWithoutDups/a.cpp
@@ -0,0 +1,29 @@
+// Copyright (C) 2017 by iamslash
+
+// Permutations without Dups: Write a method to compute all
+// permutations of a string of unique characters.
+
+#include <cstdio>
+#include <string>
+
+int N;
+std::string S;
+
+void solve(std::string s) {
+  for (int i = 0; i < s.size(); ++i) {
+    
+  }
+}
+
+int main() {
+  int T;
+  scanf("%d", &T);
+  for (int t = 0; t < T; ++t) {
+    char buf[1024];
+    scanf("%s", buf);
+    S = buf;
+    solve(S);
+  }
+  return 0;
+}
+
