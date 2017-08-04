304d1d128dd8eeca8c2f7bb855ed0dc35fd65596
iamslash <iamslash@gmail.com>
2017-08-04 11:07:35 +0900

    stack of boxes ing...

---
+++ crackingthecodinginterview/8.13_StackOfBoxes/a.cpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2017 by iamslash
+
+// Stack of Boxes: You have a stack of n boxes, with widths W i'
+// heights hi' and depths d 1? The boxes cannot be rotated and can
+// only be stacked on top of one another if each box in the stack is
+// strictly larger than the box above it in width, height. and
+// depth. Implement a method to compute the height of the tallest
+// possible stack.The height of a stack is the sum of the heights of
+// each box.
+
+
