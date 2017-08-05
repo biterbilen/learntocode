23ce69af2130a900bd868d9f24bc49cdcd00b45e
iamslash <iamslash@gmail.com>
2017-08-05 10:58:41 +0900

    boolean evaluation ing...

--- crackingthecodinginterview/8.14_BooleanEvaluation/a.cpp
+++ crackingthecodinginterview/8.14_BooleanEvaluation/a.cpp
@@ -11,7 +11,36 @@
 // countEval("1^0|0|1", false) -> 2
 // countEval("0&0&0&1^1|0", true) -> 10
 
+#include <cstdio>
+#include <vector>
+
+std::vector<char> OPERANDS;
+std::vector<char> OPERATORS;
+
+// operator_idx : operator index
+// result : operation result
+// return : profit cases
+int solve(int operator_idx, int result) {
+  return 0;
+}
+
 int main() {
+  int T;
+  scanf("%d", &T);
+  for (int t = 0; t < T; ++t) {
+    char buf1[1024];
+    char buf2[1024];
+    scanf("%s %s", buf1, buf2);
+    int n_buf1_len = strlen(buf1);
+    for (int i = 0; i < n_buf1_len; ++i) {
+      if (i % 2 == 0) {
+        OPERANDS.push_back(buf1[i]);
+      } else {
+        OPERATORS.push_back(buf1[i]);
+      }
+    }
+    printf("%d\n", solve(0, strcmp(buf2, "true") == 0 ? 1 : 0));
+  }
   return 0;
 }
 
