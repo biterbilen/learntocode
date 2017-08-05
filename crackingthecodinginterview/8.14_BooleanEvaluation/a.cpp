c84b2dee5389ef45679360d8af68ff8f9d111692
iamslash <iamslash@gmail.com>
2017-08-05 15:17:09 +0900

    boolean evaluation ing...

--- crackingthecodinginterview/8.14_BooleanEvaluation/a.cpp
+++ crackingthecodinginterview/8.14_BooleanEvaluation/a.cpp
@@ -14,32 +14,78 @@
 #include <cstdio>
 #include <vector>
 
-std::vector<char> OPERANDS;
-std::vector<char> OPERATORS;
+std::vector<char> OPS;
 
-// operator_idx : operator index
-// result : operation result
-// return : profit cases
-int solve(int operator_idx, int result) {
-  return 0;
+int CACHE[100][100][2];
+// return : profit case count
+int solve(int start, int end, int result) {
+  printf("%d %d %d\n", start, end, result);
+  // base condition
+  if (start == end - 1 && (OPS[start] == 0 || OPS[start] == 1)) {
+    return OPS[start] == result;
+  }
+  // memoization
+  int& r = CACHE[start][end][result];
+  if (r != -1)
+    return r;
+  // recursion
+  r = 0;
+  for (int i = start; i <= end; ++i) {
+    if (result == 1) {
+      switch (OPS[i]) {
+      case '&':
+        r += solve(start, i, 1) * solve(i + 1, end, 1);
+        break;
+      case '|':
+        r += solve(start, i, 1) * solve(i + 1, end, 1);
+        r += solve(start, i, 0) * solve(i + 1, end, 1);
+        r += solve(start, i, 1) * solve(i + 1, end, 0);
+        break;
+      case '^':
+        r += solve(start, i, 1) * solve(i + 1, end, 0);
+        r += solve(start, i, 0) * solve(i + 1, end, 1);
+        break;
+      }
+    } else {
+      switch (OPS[i]) {
+      case '&':
+        r += solve(start, i, 0) * solve(i + 1, end, 0);
+        r += solve(start, i, 1) * solve(i + 1, end, 0);
+        r += solve(start, i, 0) * solve(i + 1, end, 1);
+        break;
+      case '|':
+        r += solve(start, i, 0) * solve(i + 1, end, 0);
+        break;
+      case '^':
+        r += solve(start, i, 1) * solve(i + 1, end, 1);
+        r += solve(start, i, 0) * solve(i + 1, end, 0);
+        break;
+      }
+    }
+  }
+  return r;
 }
 
 int main() {
   int T;
   scanf("%d", &T);
   for (int t = 0; t < T; ++t) {
+    // init
+    for (int i = 0; i < 100; ++i) {
+      for (int j = 0; j < 100; ++j) {
+        CACHE[i][j][0] = -1;
+        CACHE[i][j][1] = -1;
+      }
+    }
     char buf1[1024];
     char buf2[1024];
     scanf("%s %s", buf1, buf2);
     int n_buf1_len = strlen(buf1);
+    OPS.clear();
     for (int i = 0; i < n_buf1_len; ++i) {
-      if (i % 2 == 0) {
-        OPERANDS.push_back(buf1[i]);
-      } else {
-        OPERATORS.push_back(buf1[i]);
-      }
+        OPS.push_back(buf1[i]);
     }
-    printf("%d\n", solve(0, strcmp(buf2, "true") == 0 ? 1 : 0));
+    printf("%d\n", solve(0, OPS.size(), strcmp(buf2, "true") == 0 ? 1 : 0));
   }
   return 0;
 }
