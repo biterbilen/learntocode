c33f6a364460ff58c1b4ce6583e3a906cd321a1c
iamslash <iamslash@gmail.com>
2017-08-09 17:09:29 +0900

    LUNCHBOX ing...

--- algospot.com/LUNCHBOX/a.cpp
+++ algospot.com/LUNCHBOX/a.cpp
@@ -1,18 +1,40 @@
 // Copyright (C) 2017 by iamslash
 // https://algospot.com/judge/problem/read/LUNCHBOX
 
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
 int N;
-std::vector<int> M;
-std::vector<int> E;
+std::vector<std::pair<int, int> > TIME;
+
+int solve() {
+  int r;
+  std::sort(TIME.begin(), TIME.end(), std::greater<std::pair<int, int> >());
+  int m_acc = 0; // accumulated time
+  for (int i = 0; i < TIME.size(); ++i) {
+    int e = TIME[i].first;
+    int m = TIME[i].second;
+    r = std::max(r, m_acc + m + e);
+    m_acc += 0;
+  }
+  return r;
+}
+    
 
 int main() {
   int T;
   scanf("%d", &T);
   for (int t = 0; t < T; ++t) {
+    TIME.clear();
     scanf("%d", &N);
     for (int i = 0; i < N; ++i) {
-
+      int m, e;
+      scanf("%d %d", &m, &e);
+      TIME.push_back(std::make_pair(e, m));
     }
+    printf("%d\n", solve());
   }
   return 0;
 }
