9dd45bc5403dfa8406f36e2d1beb15068e81f7e7
iamslash <iamslash@gmail.com>
2017-08-09 17:15:12 +0900

    STRJOIN ing...

--- algospot.com/STRJOIN/a.cpp
+++ algospot.com/STRJOIN/a.cpp
@@ -1,59 +1,29 @@
-//https://algospot.com/judge/problem/read/STRJOIN
+// Copyright (C) 2017 by iamslash
+// https://algospot.com/judge/problem/read/STRJOIN
 
 #include <cstdio>
-#include <string>
 #include <vector>
-#include <iostream>
-#include <cstring>
-#include <algorithm>
-#include <set>
 #include <queue>
-
-using namespace std;
 
 int N;
 
-int get_min_cats(const vector<int>& v_nums)
-{
-    priority_queue<int, vector<int>, greater<int> > pq;
-    for(int i=0; i<v_nums.size(); ++i)
-        pq.push(v_nums[i]);
+std::vector<int> LEN;
 
-    int r = 0;
-
-    while (pq.size() > 1)
-    {
-        int n_1 = pq.top(); pq.pop();
-        int n_2 = pq.top(); pq.pop();
-        pq.push(n_1 + n_2);
-        r += n_1 + n_2;
-    }
-
-    return r;
-    
+int solve() {
+  int r = 0;
+  return r;
 }
 
 int main() {
-    
-    int C; // number of cases
-    scanf("%d", &C);
-    
-    for(int c=0; c<C; ++c)
-    {
-        scanf("%d", &N);
-
-        vector<int> v_nums;
-
-        for (int i=0; i<N; ++i)
-        {
-            int n;
-            scanf("%d", &n);
-            v_nums.push_back(n);
-        }
-
-        printf("%d\n", get_min_cats(v_nums));
-
-    }
-
-    return 0;
+  int T;
+  scanf("%d", &T);
+  for (int t = 0; t < T; ++t) {
+    scanf("%d", &N);
+    LEN.resize(N);
+    for (int i = 0; i < N; ++i)
+      scanf("%d", &LEN[i]);
+    printf("%d\n", solve());
+  }
+  return 0;
 }
+
