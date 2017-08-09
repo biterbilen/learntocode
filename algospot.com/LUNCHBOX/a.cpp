4e1cca37703acb21fc41573671e0ccd9c812b02e
iamslash <iamslash@gmail.com>
2017-08-09 11:43:58 +0900

    LUNCHMOX ing...

--- algospot.com/LUNCHBOX/a.cpp
+++ algospot.com/LUNCHBOX/a.cpp
@@ -1,73 +1,19 @@
-//https://algospot.com/judge/problem/read/LUNCHBOX
-
-#include <cstdio>
-#include <string>
-#include <vector>
-#include <iostream>
-#include <cstring>
-#include <algorithm>
-#include <set>
-#include <queue>
-
-using namespace std;
+// Copyright (C) 2017 by iamslash
+// https://algospot.com/judge/problem/read/LUNCHBOX
 
 int N;
-
-int get_min_times(const vector<int> & v_m,
-                  const vector<int> & v_e)
-{
-    int n = v_m.size();
-
-    vector< pair<int, int> > v_order;
-
-    for(int i=0; i<n; ++i)
-    {
-        v_order.push_back( make_pair(-v_e[i], i) );
-    }
-    sort(v_order.begin(), v_order.end());
-
-    int r = 0;
-    int time_to_eat = 0;
-    for(int i=0; i<n; ++i)
-    {
-        int idx = v_order[i].second;
-        time_to_eat += v_m[idx];
-        r = max(r, time_to_eat + v_e[idx]);
-    }
-
-    return r;
-}
-
+std::vector<int> M;
+std::vector<int> E;
 
 int main() {
-    
-    int C; // number of cases
-    scanf("%d", &C);
-    
-    for(int c=0; c<C; ++c)
-    {
-        scanf("%d", &N);
-
-        vector<int> v_e; // 먹는데 걸리는 시간
-        vector<int> v_m; // 데우는데 걸리는 시간
-
-        for (int i=0; i<N; ++i)
-        {
-            int n;
-            scanf("%d", &n);
-            v_m.push_back(n);
-        }
-        
-        for (int i=0; i<N; ++i)
-        {
-            int n;
-            scanf("%d", &n);
-            v_e.push_back(n);
-        }
-        
-        printf("%d\n", get_min_times(v_m, v_e));
+  int T;
+  scanf("%d", &T);
+  for (int t = 0; t < T; ++t) {
+    scanf("%d", &N);
+    for (int i = 0; i < N; ++i) {
 
     }
-
-    return 0;
+  }
+  return 0;
 }
+
