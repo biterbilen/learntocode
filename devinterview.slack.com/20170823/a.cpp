9efc33318542dbb80441172e00f9af7b3e5ee873
iamslash <iamslash@gmail.com>
2017-08-24 00:04:15 +0900

    devinterview ing...

--- devinterview.slack.com/20170823/a.cpp
+++ devinterview.slack.com/20170823/a.cpp
@@ -32,22 +32,31 @@ class Node {
   }
 };
 
-void print_node(Node* p_node, int n_lv) {
-  if (n_lv - 1 < 0)
-    return;
-  // BFS
-  std::queue<Node*> q;
+void solve(Node* p_node, int n_lv) {
+/*
+           4
+         /   \
+        2     6
+       /     / 
+      1     5   
+      
+*/
+  // BFS                // n_lv 2
+  int n_depth = 0;      // 1
+  std::queue<Node*> q;  // 2 4
+  std::queue<Node*> qq;
   q.push(p_node);
-  int n_id = 0;
-  int n_left = pow(2, n_lv - 1) - 1;
-  int n_right = pow(2, n_lv) - 1;
-  while (!q.empty() && n_id < n_right) {
+  while (!q.empty() && n_depth < n_lv) {
     Node* p_cur = q.front(); q.pop();
-    if (n_id >= n_left)
-      printf("%d ", p_cur->m_n_val);
-    n_id++;
-    q.push(p_cur->m_p_left);
-    q.push(p_cur->m_p_right);
+    if (p_cur->m_p_left != NULL)
+      qq.push(p_cur->m_p_left);
+    if (p_cur->m_p_right != NULL)
+      qq.push(p_cur->m_p_right);
+    if (q.empty()) {
+      n_depth++;
+      q = qq;
+      qq.clear();
+    }
   }
   printf("\n");
 }
@@ -55,11 +64,11 @@ void print_node(Node* p_node, int n_lv) {
 int main() {
   Node* root = new Node(4,
                         new Node(2,
-                                 new Node(1, NULL, NULL), new Node(3, NULL, NULL)),
+                                 new Node(1, NULL, NULL), NULL),
                         new Node(6,
-                                 new Node(5, NULL, NULL), new Node(7, NULL, NULL)));
-  print_node(root, 1);
-  print_node(root, 2);
-  print_node(root, 3);
+                                 new Node(5, NULL, NULL), NULL));
+  solve(root, 1);
+  solve(root, 2);
+  solve(root, 3);
   return 0;
 }
