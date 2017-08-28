4df79841cf10337f6cd16fa39250d39d6f3b4a32
iamslash <iamslash@gmail.com>
2017-08-28 10:19:53 +0900

    devinterview ing..

---
+++ devinterview.slack.com/20170830/a.cpp
@@ -0,0 +1,9 @@
+// Copyright (C) 2017 by iamslash
+
+// Write a function to determine if any 3 integers in an array sum to
+// 0. Return the first set of 3 integers found if there are such set,
+// return false if no such set exists
+
+// e.g.
+// input = [6, -2, 5, -4, 9]
+// output = [6.-2.-4]
