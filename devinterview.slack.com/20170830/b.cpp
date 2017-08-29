dfa08188c8429823947232c33d86d9bb6ce88062
iamslash <iamslash@gmail.com>
2017-08-29 11:38:17 +0900

    devinterview ing...

--- devinterview.slack.com/20170830/b.cpp
+++ devinterview.slack.com/20170830/b.cpp
@@ -16,13 +16,62 @@
 // Output: 7
 // Explanation: TOON - POON - POIN - POIE - PLIE - PLEE - PLEA
 
+// g++ b.cpp -std=c++11 -o b.exe
 
-// 
-int solve(std::vector<std::string> v, std::string start, std::string target) {
+#include <cstdio>
+#include <vector>
+#include <set>
+#include <string>
+
+// Exhaustive search
+// O(N!)
+// ???
+int BEST = 987654321;
+bool is_adj(const std::string& a, const std::string&b) {
+  if (a.size() != b.size())
+    return false;
+  int n = 0;
+  for (int i = 0; i < a.size(); ++i) {
+    if (a[i] != b[i]) {
+      ++n;
+      if (n > 1)
+        break;
+    }
+  }
+  if (n == 1)
+    return true;
+  return false;
+}
+
+void _solve(std::set<std::string>& s, const std::string& prev, const std::string& end, int n_lv) {
+  // base condition
+  if (s.empty()) {
+    BEST = n_lv;
+    return;
+  }
+  // pruning
+  if (BEST <= n_lv)
+    return;
+  // recursion
   
 }
 
+int solve(const std::vector<std::string>& v, std::string start, std::string target) {
+  std::set<std::string> s;
+  for (const auto& e : v)
+    s.insert(e);
+  for (const auto& e : s) {
+    _solve(s, e, target, 0);
+  }
+  return BEST;
+}
+
 int main() {
-  printf("%d\n", solve({"POON", "PLEE", "SAME", "POIE", "PLEA", "PLIE", "POIN"}, "TOON", "PLEA");
+  int r = solve({"POON", "PLEE", "SAME", "POIE", "PLEA", "PLIE", "POIN"}, "TOON", "PLEA");
+  if (r == 0 || r == MAX) {
+    printf("IMPOSSIBLE\n");
+  } else {
+    printf("%d\n", r);
+  }
   return 0;
 }
