8e0e5426c02d35f896f76eec9d01e2ebdeeacb41
iamslash <iamslash@gmail.com>
2017-09-20 11:47:42 +0900

    devinterview ing...

---
+++ devinterview.slack.com/20170920/b.cpp
@@ -0,0 +1,17 @@
+// Copyright (C) 2017 by iamslash
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+
+// Question. Remove Nth node from end of list
+
+// Given a single linked list, remove the n-th node from the end of
+// list and return its head.
+
+// ex)
+//     Given linked list : 1->2->3->4->5
+//     n = 2
+
+//     Return linked list : 1->2->3->5
+
+// Notes.
+//     - Given n will always be valid
+//     - Try to do this in one pass
