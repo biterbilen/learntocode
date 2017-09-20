6380452952f1c6c5a7505b8307e7607ff428b16f
iamslash <iamslash@gmail.com>
2017-09-20 18:13:09 +0900

    devinterview ing...

--- devinterview.slack.com/20170920/a.cpp
+++ devinterview.slack.com/20170920/a.cpp
@@ -70,19 +70,104 @@ bool solve(int sum) {
 }
 
 bool solve_iter(int sum) {
+  if (N == 1)
+    return false;
   if (sum % 2 != 0)
     return false;
+  sum = sum / 2;
+  
+  // init result
+  // result[i][j] :
+  // there is a subset of NUMS[0..i] with sum equal to j ???
+  bool result[N][sum+1];
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j <= sum; ++j) {
+      result[i][j] = false;
+      if (j == 0 || j == NUMS[0])
+        result[i][j] = true;
+    }
+  }
+  
+  for (int i = 1; i < N; ++i) {
+    int num = NUMS[i];
+    for (int j = 1; j <= sum; ++j) {
+      if (num <= j) {
+        result[i][j] = result[i-1][j] || result[i-1][j-num];
+      }
+    }
+  }
 
-  //
+  // for (int i = 0; i < N; ++i) {
+  //   printf("NUMS[%2d] %2d : ", i, NUMS[i]);
+  //   for (int j = 0; j <= sum; ++j) {
+  //     printf("%2d ", result[i][j]); 
+  //   }
+  //   printf("\n");
+  // }
+  
+  return result[N-1][sum];
+}
+
+
+bool solve_iter_window(int sum) {
+  if (N == 1)
+    return false;
+  if (sum % 2 != 0)
+    return false;
+  sum = sum / 2;
+
+  bool result[2][sum+1];
+  for (int j = 0; j <= sum; ++j) {
+    result[0][j] = false;
+    if (j == 0 || j == NUMS[0])
+      result[0][j] = true;
+  }
+
+  printf("NUMS[%2d] %2d : ", 0, NUMS[0]);
+  for (int j = 0; j <= sum; ++j) {
+    printf("%2d ", result[0][j] ? 1 : 0); 
+  }
+  printf("\n");
+  
+  for (int i = 1; i < N; ++i) {
+    int num = NUMS[i];
+    int a = i % 2;
+    int b = (i+1)%2;
+
+    result[a][0] = true;
+    for (int j = 1; j <= sum; ++j) {
+      if (j < num) {
+        result[a][j] = result[b][j];
+      } else {
+        result[a][j] = result[b][j] || result[b][j-num];
+      }
+    }
+    
+    printf("NUMS[%2d] %2d : ", i, NUMS[i]);
+    for (int j = 0; j <= sum; ++j) {
+      printf("%2d ", result[a][j] ? 1 : 0); 
+    }
+    printf("\n");
+  }
+
+// 4
+// 1 5 11 5
+//
+// NUMS[ 0]  1 :  1  1  0  0  0  0  0  0  0  0  0  0
+// NUMS[ 1]  5 :  1  0  0  0  0  1  1  0  0  0  0  0
+// NUMS[ 2] 11 :  1  0  0  0  0  0  0  0  0  0  0  1
+// NUMS[ 3]  5 :  1  0  0  0  0  1  0  0  0  0  0  1
+  
+  return result[(N-1)%2][sum];
 }
 
 int main() {
   int T;
   scanf("%d", &T);
   for (int t = 0; t < T; ++t) {
-    for (int i = 0; i < 101; ++i)
-      for (int j = 0; j < 100001; ++j)
-        CACHE[i][j] = -1;
+    // for (int i = 0; i < 101; ++i)
+    //   for (int j = 0; j < 100001; ++j)
+    //     CACHE[i][j] = -1;
     scanf("%d", &N);
     NUMS.resize(N);
     int sum = 0;
@@ -90,6 +175,6 @@ int main() {
       scanf("%d", &NUMS[i]);
       sum += NUMS[i];
     }
-    printf("%s\n", solve_iter(sum) ? "YES" : "NO");
+    printf("%s\n", solve_iter_window(sum) ? "YES" : "NO");
   }
 }
--- devinterview.slack.com/20170920/b.cpp
+++ devinterview.slack.com/20170920/b.cpp
@@ -15,3 +15,30 @@
 // Notes.
 //     - Given n will always be valid
 //     - Try to do this in one pass
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+      ListNode** t1 = &head, *t2 = head;
+      for(int i = 1; i < n; ++i)
+      {
+        t2 = t2->next;
+      }
+      while(t2->next != NULL)
+      {
+        t1 = &((*t1)->next);
+        t2 = t2->next;
+      }
+      *t1 = (*t1)->next;
+      return head;
+    }
+};
+
