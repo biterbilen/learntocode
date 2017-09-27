05cf9cc80666d5fb3a6f1edcae94eaf43f13c414
iamslash <iamslash@gmail.com>
2017-09-27 15:44:08 +0900

    update devinterview

--- devinterview.slack.com/20170927/a.cpp
+++ devinterview.slack.com/20170927/a.cpp
@@ -25,35 +25,44 @@
 
 using namespace std;
 
-void print_v_bool(vector<vector<bool>> v) {
-
-}
-
 class Solution {
 public:
-    int dx[4] = {-1, 0, 0, 1};
-    int dy[4] = {0, -1, 1, 1};
+  //   o
+  // o . o
+  //   o
+  //
+  int dy[4] = {-1, 1, 0, 0};
+  int dx[4] = {0, 0, -1, 1};
     bool _exist(vector<vector<char>>& board, vector<vector<bool>>& visited, 
                 int i, int j, string word) {
-        printf("[%d][%d]\n", i, j);      
+      // for (int k = 0; k < 6 - word.size(); ++k)
+      //   printf(" ");
+      // printf("[%2d][%2d]\n", i, j);
         // base condition
-        if (i < 0 || i >= board.size() ||
-           j < 0 || j >= board[0].size())
-          return false;
-        if (visited[i][j] == true)
-          return false;
-        visited[i][j] = true;
         if (word.size() == 0)
           return true;
+        // for (int k = 0; k < 6 - word.size(); ++k)
+        //   printf(" ");
+        // printf("[%2d][%2d] %c : %c\n", i, j, board[i][j], word[0]);      
         if (board[i][j] != word[0])
           return false;
+        if (word.size() == 1)
+          return true;
 
         // recursion
-        printf("[%d][%d] %c\n", i, j, word[0]);
         for (int k = 0; k < 4; ++k) {
-          if (_exist(board, visited,
-                     i+dx[k], i+dy[k], word.substr(1)))
-            return true;
+          int ny = i + dy[k];
+          int nx = j + dx[k];
+          if (ny < 0 || ny >= board.size() ||
+           nx < 0 || nx >= board[0].size())
+            continue;
+          if (visited[ny][nx] == false) {
+            visited[ny][nx] = true;
+            if (_exist(board, visited,
+                       ny, nx, word.substr(1)))
+              return true;
+            visited[ny][nx] = false;
+          }
         }
         return false;
     }
@@ -64,11 +73,13 @@ public:
                                  vector<bool>(board[0].size(), false));
         for (int i = 0; i < board.size(); ++i) {
             for (int j = 0; j < board[i].size(); ++j) {
-              if ((visited[i][j] == false) &&
-                  (word.size() > 0) &&
-                  (board[i][j] == word[0]) &&
-                  _exist(board, visited, i, j, word.substr(1)))
+              // printf("%d, %d\n", i, j);
+              if (visited[i][j] == false) {
+                visited[i][j] = true;
+                if (_exist(board, visited, i, j, word))
                     return true;
+                visited[i][j] = false;
+              }
             }
         }
         return false;
@@ -76,9 +87,13 @@ public:
 };
 
 int main() {
-  vector<vector<char>> b = {{'A', 'B', 'C', 'E'},
-                            {'S', 'F', 'C', 's'},
-                            {'A', 'D', 'E', 'E'}};
   Solution s;
-  printf("%s", s.exist(b, "ABCCED") ? "true" : "false");
+  // vector<vector<char>> b = {{'A', 'B', 'C', 'E'},
+  //                           {'S', 'F', 'C', 's'},
+  //                           {'A', 'D', 'E', 'E'}};
+  // printf("%s", s.exist(b, "ABCCED") ? "true" : "false");
+  // vector<vector<char>> b = {{'a'}};
+  // printf("%s", s.exist(b, "a") ? "true" : "false");
+  vector<vector<char>> b = {{'a', 'b'}};
+  printf("%s", s.exist(b, "ba") ? "true" : "false");
 }
