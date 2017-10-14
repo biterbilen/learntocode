19eb1469e139680bfeb2f66dbfea930d64671e79
iamslash <iamslash@gmail.com>
2017-10-14 12:00:03 +0900

    extended euclidean ing...

--- fundamentals/numbertheory/exeuclideanalgorithm/a.cpp
+++ fundamentals/numbertheory/exeuclideanalgorithm/a.cpp
@@ -4,21 +4,24 @@
 #include <cstdio>
 #include <cstdlib>
 
+#include <cstdio>
+#include <cstdlib>
+
 int xgcd(int a, int b, int& x, int &y) {
     // baseb condition
     if (b == 0) {
         x = 1;
         y = 0;
+        printf("%d = %d * %d + %d * %d\n", a, a, x, b, y); 
         return a;
     }
 
     // recursion
     int x1, y1;
     int gcd = xgcd(b, a%b, x1, y1);
-    printf("%d = %d * %d + %d * %d\n", gcd, a, x1, b, y1);
     x = y1;
     y = x1 - a/b * y1;
-
+    printf("%d = %d * %d + %d * %d\n", gcd, a, x, b, y);
     return gcd;
 }
 
@@ -26,13 +29,12 @@ int main() {
     int a = 1020, b = 790;
     int x, y;
     int gcd = xgcd(a, b, x, y);
-    printf("%d = %d * %d + %d * %d\n", gcd, a, x, b, y);
     return 0;
 }
 
-// 10 = 30 * 1 + 10 * 0
-// 10 = 100 * 0 + 30 * 1
-// 10 = 230 * 1 + 100 * -3
-// 10 = 790 * -3 + 230 * 7
-// 10 = 1020 * 7 + 790 * -24
+// 10 = 10 * 1 + 0 * 0
+// 10 = 30 * 0 + 10 * 1
+// 10 = 100 * 1 + 30 * -3
+// 10 = 230 * -3 + 100 * 7
+// 10 = 790 * 7 + 230 * -24
 // 10 = 1020 * -24 + 790 * 31
