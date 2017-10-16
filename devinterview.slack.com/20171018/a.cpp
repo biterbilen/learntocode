06ed33663d6c89dc5ccd1d343e7aecfdb622ff41
iamslash <iamslash@gmail.com>
2017-10-16 15:04:31 +0900

    devinterview added

---
+++ devinterview.slack.com/20171018/a.cpp
@@ -0,0 +1,40 @@
+//https://leetcode.com/problems/decode-ways/description/
+
+// A message containing letters from A-Z is being encoded to numbers
+// using the following mapping:
+
+// 'A' -> 1
+// 'B' -> 2
+// ...
+// 'Z' -> 26
+// Given an encoded message containing digits, determine the total
+// number of ways to decode it.
+
+// For example,
+// Given encoded message "12", it could be decoded as "AB" (1 2) or
+// "L" (12).
+
+// The number of ways decoding "12" is 2.
+
+
+// [12:22] 
+// class Solution {
+// public:
+//    int numDecodings(string s) {
+       
+//    }
+// };
+
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        
+    }
+};
+
+int main() {
+  Solution s;
+  printf("%d\n", s.sumDecodings("12"));  
+  return 0;
+}
---
+++ devinterview.slack.com/20171018/b.cpp
@@ -0,0 +1,26 @@
+
+// Find nCr for given n and r.
+
+// Input:
+// First line contains no of test cases T, for every test case 2
+// integers as inputs (n,r).
+
+
+// Output:
+// Compute and print the value in seperate line. Modulus your output
+// to 10^9+7. If n
+
+
+// Constraints:
+// 1<=T<=50
+// 1<=n<=1000
+// 1<=r<=800
+
+
+// Example:
+// Input:
+// 1
+// 3 2
+
+// Output:
+// 3
