4edbc17bbd8409776b6b3b0924f9eec4061e8d00
David Sun <iamslash@gmail.com>
2017-10-19 07:45:25 +0900

    vector ing...

--- fundamentals/computationalgeometry/vector/a.cpp
+++ fundamentals/computationalgeometry/vector/a.cpp
@@ -7,20 +7,59 @@
 
 #include <cstdio>
 #include <cmath>
+#include <string>
 
 const double PI = 2.0 * acos(0.0);
 
 struct vector2 {
-double   x, y;
-explicit vector2(double x_ = 0, double y_ = 0) : x(x_), y(y_) {}
-    bool operator == (const vector2& rhs) const {
-return x == rhs.x && y == rhs.y;
-}
-        
+  double   x, y;
+  explicit vector2(double x_ = 0, double y_ = 0) : x(x_), y(y_) {}
+  bool operator == (const vector2& rhs) const {
+    return x == rhs.x && y == rhs.y;
+  }
+  // ???
+  bool operator < (const vector2& rhs) const {
+    return x != rhs.x ? x < rhs.x : y < rhs.y;
+  }
+  vector2 operator + (const vector2& rhs) const {
+    return vector2(x + rhs.x, y + rhs.y);
+  }
+  vector2 operator - (const vector2& rhs) const {
+    return vector2(x - rhs.x, y - rhs.y);
+  }
+  vector2 operator * (double rhs) const {
+    return vector2(x * rhs, y * rhs);
+  }
+  double norm() const { return hypot(x, y); }
+  vector2 normalize() const {
+    return vector2(x / norm(), y / norm());
+  }
+  // angle from x-axis ccw (count clock wise)
+  double polar() const {
+    return fmod(atan2(y, x) + 2 * PI, 2 * PI);
+  }
+  double dot(const vector2& rhs) const {
+    return x * rhs.x + y * rhs.y;
+  }
+  double cross(const vector2& rhs) const {
+    return x * rhs.y - rhs.x * y;
+  }
+  // a vector projected to this vector
+  vector2 project(const vector2& rhs) const {
+    vector2 r = rhs.normalize();
+    return r * r.dot(*this);
+  }
+  
+  std::string tostring() const {
+    char buf[32] = {0, };
+    snprintf(buf, sizeof(buf), "(%0.2lf, %0.2lf)", x, y);
+    return buf;
+  }
 };
 
 
 int main() {
-  vector2 v;
+  vector2 v(2.0, 3.0);
+  printf("%s\n", v.tostring().c_str());
   return 0;
 }
