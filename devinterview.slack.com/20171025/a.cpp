7acc92097722220db24d4b6a8596d937ebe4dc77
iamslash <iamslash@gmail.com>
2017-10-25 12:07:31 +0900

    devinterview ing...

--- devinterview.slack.com/20171025/a.cpp
+++ devinterview.slack.com/20171025/a.cpp
@@ -4,22 +4,30 @@
 #include <queue>
 #include <vector>
 
-
-
 class Solution {
  private:
  public:
   int solve(int N, int B, const std::vector<int>& a) {
-    std::vector<int> clinic(a.size(), 1);
     std::vector<int> load(a.size());  // m_load[i] = population / clinic
+    std::vector<int> clinic(a.size(), 1);
+    auto mycomp = [load](const int& idx1, const int& idx2) {
+      return load[idx1] < load[idx2];
+    };    
+    std::priority_queue<int, std::vector<int>, decltype(mycomp)> pq(mycomp);
+
     for (int i = 0; i < a.size(); ++i) {
-      load[i] = a[i] / clinic[i];
+      load[i] = a[i] / clinic[i];      
+      pq.push(i);
     }
-    std::priority_queue<int, std::vector<int>, decltype(mycomp)> pq()
     for (int i = 0; i < B - N; ++i) {
-      
+      // j is index of city which has the biggest load 
+      int j = pq.top(); pq.pop();
+      clinic[j] += 1;
+      load[j] = a[j] / clinic[j];
+      pq.push(j);
     }
-    return r;
+    
+    return load[pq.top()];
   }
 };
 
