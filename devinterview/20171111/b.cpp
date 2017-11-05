2a689fad6272d6a918ab5b9b3bd898d949faba26
David Sun <iamslash@gmail.com>
2017-11-05 18:14:13 +0900

    update devinterview

--- devinterview/20171111/b.cpp
+++ devinterview/20171111/b.cpp
@@ -1,3 +1,29 @@
 // Copyright (C) 2017 by iamslash
 
 // https://algospot.com/judge/problem/read/POTION
+
+// There are n bottles. Every bottle has p_i spoonfuls of different
+// kinds of liquids. (0 <= i < n). Every each liquid changes to
+// solid when collected in r_i spoonfuls.  You need to change the
+// liquid of all bottles to solid using q_i spoonfuls.  You must use
+// minimum q_i spoonfuls.
+//
+// For examples:
+//
+// 4 bottles
+// 4 6 2 4 for r_i
+// 6 4 2 4 for p_i
+//
+// 0 5 1 2 for q_i
+//
+// 4 bottles
+// 4 6 2 4 for r_i
+// 7 4 2 4 for p_i
+//
+// 1 8 2 4 for q_i
+//
+// 3 bottles
+// 4 5 6 for r_i
+// 1 2 3 for p_i
+//
+// 3 3 3 for q_i
