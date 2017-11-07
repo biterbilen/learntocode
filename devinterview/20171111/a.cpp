37f35c50873281de5ea39f6531eb2de794e7a1ea
iamslash <iamslash@gmail.com>
2017-11-07 18:06:03 +0900

    devinterview ing...

--- devinterview/20171111/a.cpp
+++ devinterview/20171111/a.cpp
@@ -19,3 +19,38 @@
 
 // if there are multiple such windows, you are guaranteed that there
 // will always be only one unique minimum window in S.
+
+//  t
+// “ADOBECODEBANC”
+// "ABC"
+class Solution {
+ public:
+  std::string minWindow(std::string s, std::string t) {
+    int matched_cnt = 0;
+    int s_cnt[256] = {0,};
+    int t_cnt[256] = {0,};
+    int fr_idx_min = 0;
+    int len_min = s.size();
+
+    // validate s
+    if (s.size() < t.size())
+      return "";
+
+    // set t_cnt
+    for (int i = 0; i < t.size(); ++i) {
+      t_cnt[i]++;
+    }
+
+    // traverse s to make a minim window
+    for (int to_idx_min = 0; to_idx_min < s.size(); ++to_idx_min) {
+      
+    }
+
+    return s.substr(fr_idx_min, len_min);
+  }
+};
+
+int main() {
+  S s;
+  printf("%s\n", s.minWindow("ADOBECODEBANC", "ABC").c_str());
+}
