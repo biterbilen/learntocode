6c08f0286504ae721bd7f71c6872115be165270c
iamslash <iamslash@gmail.com>
2017-11-08 11:51:47 +0900

    devinterview ing...

--- devinterview/20171111/a.cpp
+++ devinterview/20171111/a.cpp
@@ -1,3 +1,4 @@
+
 // Copyright (C) 2017 by iamslash
 // https://leetcode.com/problems/minimum-window-substring/description/
 // http://www.geeksforgeeks.org/find-the-smallest-window-in-a-string-containing-all-characters-of-another-string/
@@ -24,51 +25,66 @@
 // “ADOBECODEBANC”
 // "ABC"
 
-const int MAX_INT = 9876543210;
+#include <cstdio>
+#include <string>
+#include <vector>
+
+
+const int MAX_INT = 987654321;
 
 class Solution {
  public:
   std::string minWindow(std::string s, std::string t) {
-    int matched_cnt = 0;
+    int matched = 0;
     int s_cnt[256] = {0,};
     int t_cnt[256] = {0,};
-    int fr_idx_min = MAX_INT;
+    int fr_idx_min = -1;
     int len_min = MAX_INT;
     int fr_idx = 0;
 
-    // validate s
+    // validate s length
     if (s.size() < t.size())
       return "";
 
     // set t_cnt
     for (int i = 0; i < t.size(); ++i) {
       t_cnt[t[i]]++;
-      s_cnt[s[i]]++;
     }
 
     // traverse s to make a min window
-    for (int to_idx = t.size(); to_idx < s.size(); ++to_idx) {
-      // move right idx of window
-      s_cnt[s[to_idx]]++;
+    for (int to_idx = 0; to_idx < s.size(); ++to_idx) {
+      // c is in t
+      char to_c = s[to_idx];
+      s_cnt[to_c]++;
+      if (t_cnt[to_c] != 0 && s_cnt[to_c] < t_cnt[to_c])
+        matched++;
       
-      // move left idx of window
-      if (matched_cnt == t.size()) {
-
+      if (matched == t.size()) {
+        // move left idx of window
+        char fr_c = s[fr_idx];
+        while (t[fr_c] == 0 || s[fr_c] > t[fr_c]) {
+          if (s[fr_c] > t[fr_c])
+            s[fr_c]--;
+          fr_idx++;
+        }
+        // set len_min
+        int len_cur = to_idx - fr_idx + 1;
+        if (len_cur < len_min) {
+          fr_idx_min = fr_idx;
+          len_min = len_cur;
+        }
       }
-
-      // set len_min
-      if (len_min > (to_idx - fr_idx + 1)) {
-        fr_idx_min = fr_idx;
-        len_min = to_dix - fr_idx + 1;
-      }
-      
     }
+
+    // There is no window
+    if (fr_idx_min < 0)
+      return "";
 
     return s.substr(fr_idx_min, len_min);
   }
 };
 
 int main() {
-  S s;
+  Solution s;
   printf("%s\n", s.minWindow("ADOBECODEBANC", "ABC").c_str());
 }
