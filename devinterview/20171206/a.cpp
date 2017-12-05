6803050c5d3c63aa3e7d3559b01ddcaa88324ea9
iamslash <iamslash@gmail.com>
2017-12-05 18:13:36 +0900

    devinv ing...

--- devinterview/20171206/a.cpp
+++ devinterview/20171206/a.cpp
@@ -1,7 +1,6 @@
 // Copyright (C) 2017 by iamslash
 
-// https://practice.geeksforgeeks.org/problems/n-queen-problem/0
-// http://www.geeksforgeeks.org/backtracking-set-3-n-queen-problem/
+// https://www.hackerrank.com/challenges/bear-and-steady-gene/problem
 
 int main() {
 
