ba77ff996960b7003935cee77c698234120af9a7
iamslash <iamslash@gmail.com>
2017-12-05 11:49:58 +0900

    added devinv

---
+++ devinterview/20171206/a.cpp
@@ -0,0 +1,8 @@
+// Copyright (C) 2017 by iamslash
+
+// https://practice.geeksforgeeks.org/problems/n-queen-problem/0
+// http://www.geeksforgeeks.org/backtracking-set-3-n-queen-problem/
+
+int main() {
+
+}
