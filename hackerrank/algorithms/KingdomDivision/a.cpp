387949c0d6b7c4b7a967c7840a0a60add78f063f
iamslash <iamslash@gmail.com>
2018-01-11 16:15:56 +0900

    update kingdom division

--- hackerrank/algorithms/KingdomDivision/a.cpp
+++ hackerrank/algorithms/KingdomDivision/a.cpp
@@ -11,27 +11,55 @@ const int MOD = 1000000007;
 std::vector<std::vector<int>> adj;
 int64_t CACHE[100005][2][2];
 
-int64_t solve(int node, int parent, int idx, int color, int ally, int lv) {
-  for (int i = 0; i < lv; ++i) {
-    printf("-");
-  }
-  printf("%2d %2d %2d %2d %2d\n", node+1, parent+1, idx, color, ally);
+int64_t solve(int here, int parent, int idx, int color, int ally, int lv) {
+  // for (int i = 0; i < lv; ++i) {
+  //   printf("-");
+  // }
+  // printf("%2d %2d %2d %2d %2d\n", here+1, parent+1, idx, color, ally);
   // base condition
-  if (idx == adj[node].size())
+  if (idx == adj[here].size())
     return ally;
-  int child = adj[node][idx];
+  int child = adj[here][idx];
   if (child == parent)
-    return solve(node, parent, idx+1, color, ally, lv+1);
+    return solve(here, parent, idx+1, color, ally, lv+1);
   int64_t& r = CACHE[child][color][ally];
   if (r != -1)
     return r;
   // recursion
-  r = (solve(child, node, 0, color, 1, lv+1) *
-       solve(node, parent, idx+1, color, 1, lv+1)) % MOD;
-  r += (solve(child, node, 0, 1-color, 0, lv+1) *
-        solve(node, parent, idx+1, color, ally, lv+1)) % MOD;
+  r = (solve(child, here, 0, color, 1, lv+1) *
+       solve(here, parent, idx+1, color, 1, lv+1)) % MOD;
+  r += (solve(child, here, 0, 1-color, 0, lv+1) *
+        solve(here, parent, idx+1, color, ally, lv+1)) % MOD;
   return r;
 }
+
+// in case of d.txt fail
+//
+// int64_t solve(int u, int p, int color, int ally, int lv) {
+//   // debug
+//   for (int i = 0; i < lv; ++i) {
+//     printf("-");
+//   }
+//   printf("%2d %2d %2d %2d\n", u+1, p+1, color, ally);
+
+//   // base condition
+//   if (adj[u].size() == 0)
+//     return ally;
+
+//   // memoization
+//   int64_t& r = CACHE[u][color][ally];
+//   if (r != -1)
+//     return r;
+//   r = 1;
+
+//   for (int i = 0; i < adj[u].size(); ++i) {
+//     int v = adj[u][i];
+//     int rr = solve(v, u, color, 0, lv+1);
+//     rr += solve(v, u, 1-color, 1, lv+1);
+//     r *= rr;
+//   }
+//   return r;
+// }
 
 int main() {
   scanf("%d", &N);
@@ -39,7 +67,6 @@ int main() {
 
   for (int i = 0; i < N; ++i) {
     CACHE[i][0][0] = CACHE[i][0][1] = CACHE[i][1][0] = CACHE[i][1][1] = -1;
-        
   }
   
   for (int i = 0; i < N - 1; ++i) {
