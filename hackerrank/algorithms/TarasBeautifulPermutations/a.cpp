2b30ae9083b6d383458d03d4ddfbb0ef9b223316
iamslash <iamslash@gmail.com>
2018-01-17 12:38:59 +0900

    beautiful permutations ing...

--- hackerrank/algorithms/TarasBeautifulPermutations/a.cpp
+++ hackerrank/algorithms/TarasBeautifulPermutations/a.cpp
@@ -5,8 +5,10 @@
 #include <cstdio>
 #include <vector>
 #include <algorithm>
+#include <set>
 
 const int MOD = 1000000007;
+int CACHE[2048][2048]; // CACHE[i][j] = i-th order, j number
 
 int N;
 std::vector<int> A;
@@ -14,47 +16,41 @@ std::vector<int> A;
 // examples
 // 1 1 2
 //
-// 1 1 2 
-// 1 2 1
-// 1 1 2 .
-// 1 2 1
-// 2 1 1
-// 2 1 1 .
+// 1 1 2 X
+// 1 2 1 
+// 1 1 2 X
+// 1 2 1 
+// 2 1 1 X
+// 2 1 1 X
 
-// unique permutation count = n! - 2^(duplicated number count)
-int solve() {
-  //
-  if (N == 1)
+int solve(const std::set<int> s) {
+  if (s.size() == 2)
     return 1;
-  int n_factorial = 1;
-  int n_dupnumcnt = 0;
-  //
-  for (int i = 1; i < N; ++i) {
-    n_factorial = n_factorial * (i+1) % MOD;
-    if (A[i-1] == A[i])
-      ++n_dupnumcnt;
+  int r = 1;
+  for (int i = 0; i < s.size(); ++i) {
+    r = r * (i+1) % MOD;
   }
-  //
-  int a = n_factorial;
-  int b = 1 << n_dupnumcnt;
-  int c = n_dupnumcnt * (N - 1);
-  printf("  %d %d\n", n_factorial, n_dupnumcnt);
-  printf("  %d %d %d\n", a, b, c);
-  
-  return (a / b) - c;
+  return r;
 }
 
 int main() {
   int T;
   scanf("%d", &T);
   for (int t = 0; t < T; ++t) {
+    for (int i = 0; i < 2000; ++i) {
+      for (int j = 0; j < 2000; ++j) {
+        CACHE[i][j] = -1;
+      }
+    }
     scanf("%d", &N);
     A.resize(N);
     for (int i = 0; i < N; ++i) {
       scanf("%d", &A[i]);
     }
     std::sort(A.begin(), A.end());
-    printf("%d\n", solve());
+    // std::erase(std::unique(A.begin(), A.end()), A.end());
+    std::set<int> s(A.begin(), A.end());
+    printf("%d\n", solve(s));
   }
   return 0;
 }
