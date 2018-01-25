33ae4346983c39d8bc17654deaa6151934999872
iamslash <iamslash@gmail.com>
2018-01-25 18:46:36 +0900

    update bst insert

--- fundamentals/tree/binarysearchtree/a.cpp
+++ fundamentals/tree/binarysearchtree/a.cpp
@@ -6,8 +6,14 @@ struct Node {
   int val;
   Node* left;
   Node* right;
+  
   Node() {
     val = -1;
+    left = NULL;
+    right = NULL;
+  }
+  explicit Node(int _val) {
+    val = _val;
     left = NULL;
     right = NULL;
   }
@@ -20,13 +26,39 @@ struct Node {
 };
 
 Node* Search(Node* root, int val) {
+  // base condition
   if (root == NULL || root->val == val)
     return root;
+  // recursion
   if (root->val < val)
     return Search(root->right, val);
   return Search(root->left, val);
 }
 
+Node* Insert(Node* root, int val) {
+  // base condition
+  if (root == NULL)
+    return new Node(val);
+  // recursion
+  if (val < root->val)
+    root->left = Insert(root->left, val);
+  else
+    root->right = Insert(root->right, val);
+  return root;
+}
+
+
+
 int main() {
+  Node* root = new Node();
+
+  root = Insert(root, 50);
+  root = Insert(root, 30);
+  root = Insert(root, 20);
+  root = Insert(root, 40);
+  root = Insert(root, 70);
+  root = Insert(root, 60);
+  root = Insert(root, 80);
+
   return 0;
 }
