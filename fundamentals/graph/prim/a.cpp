75260b0195d28822198f71edf9b3138cfe61adbd
David Sun <iamslash@gmail.com>
2018-02-08 16:09:19 +0900

    prim ing...

--- fundamentals/graph/prim/a.cpp
+++ fundamentals/graph/prim/a.cpp
@@ -8,9 +8,8 @@ const int MAXI = 987654321;
 const int MAXV = 100;
 int V = 5;
 
-int prim(int** graph) {
+int prim(int** graph, std::vector<std::pair<int, int>>& mst) {
   int r = 0;
-  std::vector<std::pair<int, int>> mst;
   std::vector<bool> added(V, false);
   std::vector<int> minweight(V, MAXI);
   std::vector<int> parent(V, -1);
@@ -19,7 +18,18 @@ int prim(int** graph) {
   parent[0] = 0;
 
   for (int i = 0; i < V; ++i) {
-
+    int here = -1;
+    for (int j = 0; j < V; ++j) {
+      if (!added[j] && (here == -1 || minweight[here] > minweight[j])) {
+        here = j;
+      }
+    }
+    // found here with minimum weight
+    if (parent[here] != here) {
+      mst.emplace_back(parent[here], here);
+    }
+    r += minweight[here];
+    
   }
   
   return r;
@@ -31,7 +41,8 @@ int main() {
                            {0, 3, 0, 0, 7},
                            {6, 8, 0, 0, 9},
                            {0, 5, 7, 9, 0},};
-  printf("%d\n", prim(graph));
+  std::vector<std::pair<int, int>> mst;
+  printf("%d\n", prim(graph, mst));
   return 0;
 }
 
