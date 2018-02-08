34cf35e9012c3285a8d35e4219132bdea064389c
David Sun <iamslash@gmail.com>
2018-02-08 16:38:44 +0900

    prim return fail

--- fundamentals/graph/prim/a.cpp
+++ fundamentals/graph/prim/a.cpp
@@ -8,35 +8,45 @@ const int MAXI = 987654321;
 const int MAXV = 100;
 int V = 5;
 
-int prim(int** graph, std::vector<std::pair<int, int>>& mst) {
+int prim(int (&graph)[5][5],
+         std::vector<std::pair<int, int>>& mst) {
   int r = 0;
   std::vector<bool> added(V, false);
-  std::vector<int> minweight(V, MAXI);
+  std::vector<int> mincost(V, MAXI);
   std::vector<int> parent(V, -1);
 
-  minweight[0] = 0;
-  parent[0] = 0;
+  added[0]     = true;
+  mincost[0] = 0;
+  parent[0]    = 0;
 
   for (int i = 0; i < V; ++i) {
-    int here = -1;
-    for (int j = 0; j < V; ++j) {
-      if (!added[j] && (here == -1 || minweight[here] > minweight[j])) {
-        here = j;
+    int u = -1;
+    // find u with minimum weight among nodes not visited
+    for (int v = 0; v < V; ++v) {
+      if (!added[v] && (u == -1 || mincost[u] > mincost[v])) {
+        u = v;
       }
     }
-    // found here with minimum weight
-    if (parent[here] != here) {
-      mst.emplace_back(parent[here], here);
+    // start node - start node does not need
+    if (parent[u] != u) {
+      mst.emplace_back(parent[u], u);
     }
-    r += minweight[here];
-    
+    r += mincost[u];
+    // find neighbor nodes
+    for (int v = 0; v < V; ++v) {
+      int w = graph[u][v];
+      if (w && added[v] == false && w < mincost[v]) {
+        parent[v] = u;
+        mincost[v] = w;
+      }
+    }
   }
   
   return r;
 }
 
 int main() {
-  int graph[V][V] = {{0, 2, 0, 6, 0},
+  int graph[5][5] = {{0, 2, 0, 6, 0},
                            {2, 0, 3, 8, 5},
                            {0, 3, 0, 0, 7},
                            {6, 8, 0, 0, 9},
