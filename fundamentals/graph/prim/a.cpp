1bee52bcb58d274848d36675d1105edc945c82d8
David Sun <iamslash@gmail.com>
2018-02-09 12:30:45 +0900

    update prim

--- fundamentals/graph/prim/a.cpp
+++ fundamentals/graph/prim/a.cpp
@@ -11,31 +11,32 @@ int V = 5;
 int prim(int (&graph)[5][5],
          std::vector<std::pair<int, int>>& mst) {
   int r = 0;
-  std::vector<bool> added(V, false);
+  
+  std::vector<bool> added(V, false);  // is added to mst
   std::vector<int> mincost(V, MAXI);
   std::vector<int> parent(V, -1);
 
-  added[0]     = true;
-  mincost[0] = 0;
+  mincost[0]   = 0;
   parent[0]    = 0;
 
   for (int i = 0; i < V; ++i) {
     int u = -1;
     // find u with minimum weight among nodes not visited
     for (int v = 0; v < V; ++v) {
-      if (!added[v] && (u == -1 || mincost[u] > mincost[v])) {
+      if (added[v] == false && (u == -1 || mincost[v] < mincost[u]))
         u = v;
-      }
     }
+    
     // start node - start node does not need
     if (parent[u] != u) {
       mst.emplace_back(parent[u], u);
     }
+    added[u] = true;
     r += mincost[u];
     // find neighbor nodes
     for (int v = 0; v < V; ++v) {
       int w = graph[u][v];
-      if (w && added[v] == false && w < mincost[v]) {
+      if (added[v] == false && w > 0 && w < mincost[v]) {
         parent[v] = u;
         mincost[v] = w;
       }
@@ -53,6 +54,9 @@ int main() {
                            {0, 5, 7, 9, 0},};
   std::vector<std::pair<int, int>> mst;
   printf("%d\n", prim(graph, mst));
+  for (const auto& e : mst) {
+    printf("%d - %d\n", e.first, e.second);
+  }
   return 0;
 }
 
