8bc174db47df56d6ba6f007317760ff4a77685b3
David Sun <iamslash@gmail.com>
2018-02-11 22:40:59 +0900

    update nqueen backtracking bitmastk

--- algospot.com/NQUEEN/b.cpp
+++ algospot.com/NQUEEN/b.cpp
@@ -10,19 +10,19 @@ int N;
 int BEST = 0;
 int64_t END = 0;
 
-void nqueen(int64_t row, int64_t ld, int64_t rd) {
+void nqueen(int64_t vr, int64_t ld, int64_t rd) {
   // base condition
-  if (row == END) {
+  if (vr == END) {
     BEST++;
     return;
   }
   // recursion
-  int64_t pos = END & (~(row | ld | rd));
-  while (pos != 0) {
+  int64_t last = END & (~(vr | ld | rd));
+  while (last != 0) {
     // pick up right most one
-    int64_t p = pos & (-pos);
-    pos -= p;
-    nqueen(row + p, (ld + p) << 1, (rd + p) >> 1);
+    int64_t pos = last & (-last);
+    last -= pos;
+    nqueen(vr + pos, (ld + pos) << 1, (rd + pos) >> 1);
   }
 }
 
