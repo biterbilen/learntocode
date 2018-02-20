eb8cb4163ede6cbc723d4e86e3a708b6279ac1cf
David Sun <iamslash@gmail.com>
2018-02-20 21:29:05 +0900

    update maximum product subarray, word break

--- leetcode/WordBreak/a.cpp
+++ leetcode/WordBreak/a.cpp
@@ -4,20 +4,34 @@
 #include <cstdio>
 #include <vector>
 #include <string>
+#include <cstring>
 
 using namespace std;
 
 class Solution {
 public:
-  bool wordBreak(string s, vector<string>& wordDict) {
-    return false;
+  bool wordBreak(string s, vector<string>& d) {
+    std::vector<bool> f(s.size() + 1);
+    f[s.size()] = true;
+    for (int i = s.size() - 1; i >= 0; --i) {
+      for (const auto& w : d) {
+        if (i + w.size() <= s.size() &&
+            f[i + w.size()] &&
+            !strncmp(&s[i], w.c_str(), w.size())) {
+          f[i] = true;
+          break;
+        }
+      }
+    }
+    return f[0];
   }
 };
 
 
 int main() {
   Solution s;
-  std::string a = "leetcode";
+  // std::string a = "leetcode";
+  std::string a = "leecode";
   std::vector<std::string> b = {"leet", "code"};
   printf("%d\n", s.wordBreak(a, b));
   return 0;
