de1d167bc21bc0dca6ba0d6bfa40244582e69e4d
David Sun <iamslash@gmail.com>
2018-02-21 22:50:19 +0900

    added better space complexity

--- hackerrank/algorithms/MissingNumbers/a.cpp
+++ hackerrank/algorithms/MissingNumbers/a.cpp
@@ -8,6 +8,34 @@
 
 int N, M;
 
+// std::map<int, int> S;
+
+
+// int main() {
+//   int xmax = 0;
+//   int xmin = 987654321;
+//   scanf("%d", &N);
+//   for (int i = 0; i < N; ++i) {
+//     int x;
+//     scanf("%d", &x);
+//     S.find(x) == S.end() ? S[x] = 1 : S[x]++;
+//   }
+//   scanf("%d", &M);
+//   for (int i = 0; i < M; ++i) {
+//     int x;
+//     scanf("%d", &x);
+//     S.find(x) == S.end() ? S[x] = -1 : S[x]--;
+//     xmin = std::min(xmin, x);
+//     xmax = std::max(xmax, x);
+//   }
+//   for (auto it = S.begin(); it != S.end(); ++it) {
+//     if (*it.second != 0) {
+//       printf("%d ", *it.first);
+//     }
+//   }
+//   return 0;
+// }
+
 int NS[10001] = {0, };
 int MS[10001] = {0, };
 
