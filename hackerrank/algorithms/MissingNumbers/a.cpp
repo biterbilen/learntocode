ee9c739bc2dd324cffddfdc40c40b198a1b81c7e
David Sun <iamslash@gmail.com>
2018-02-21 21:53:09 +0900

    removed comments

--- hackerrank/algorithms/MissingNumbers/a.cpp
+++ hackerrank/algorithms/MissingNumbers/a.cpp
@@ -8,40 +8,8 @@
 
 int N, M;
 
-// std::set<int> solve(std::vector<int>& ln,
-//                        std::vector<int>& lm) {
-//   std::set<int> r;
-
-//   std::sort(ln.begin(), ln.end());
-//   std::sort(lm.begin(), lm.end());
-  
-//   for (int i = 0; i < ln.size(); ++i) {
-//     if (ln[i] != lm[i]) {
-//       r.insert(lm[i]);
-//       lm.erase(lm.begin()+i);
-//     }
-//   }
-//   return r;
-// }
-
-// int main() {
-//   scanf("%d", &N);
-//   std::vector<int> ln(N);
-//   for (int i = 0; i < N; ++i)
-//     scanf("%d", &ln[i]);
-//   scanf("%d", &M);
-//   std::vector<int> lm(M);
-//   for (int i = 0; i < M; ++i)
-//     scanf("%d", &lm[i]);
-//   std::set<int> r = solve(ln, lm);
-//   for (auto it = r.begin(); it != r.end(); ++it)
-//     printf("%d ", *it);
-  
-//   return 0;
-// }
-
-int NS[10001] = {0,};
-int MS[10001] = {0,};
+int NS[10001] = {0, };
+int MS[10001] = {0, };
 
 int main() {
   int xmax = 0;
