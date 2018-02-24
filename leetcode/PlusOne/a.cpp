da0b0a0d4a56c7b305b4758090069a4143ea4733
David Sun <iamslash@gmail.com>
2018-02-24 17:05:45 +0900

    kick off plus one

---
+++ leetcode/PlusOne/a.cpp
@@ -0,0 +1,14 @@
+/* Copyright (C) 2018 by iamslash */
+
+#include <cstdio>
+#include <vector>
+
+class Solution {
+public:
+  std::vector<int> plusOne(std::vector<int>& digits) {
+  }
+}; 
+
+int main() {
+  return 0;
+}
