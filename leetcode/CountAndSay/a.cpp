cc0f0f2c64b4961b03a4a1f21f395ca874d65293
David Sun <iamslash@gmail.com>
2018-03-07 07:40:24 +0900

    cc

--- leetcode/CountAndSay/a.cpp
+++ leetcode/CountAndSay/a.cpp
@@ -1,15 +1,41 @@
 // Copyright (C) 2018 by iamslash
 // https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/886/
 
+#include <cstdio>
+#include <string>
+
+
 class Solution {
+ private:
+  std::string _countAndSay(std::string s) {
+    std::string r;
+    int ccnt = 1;  // count of c
+    char c = s[0];
+    for (int i = 0; i < s.size(); ++i) {
+      if (s[i] == c) {
+        ccnt++;
+      } else {
+        r += '0' + ccnt;
+        r += c;
+        ccnt = 1;
+        c = s[i];
+      }
+    }
+    return r;
+  }
+
  public:
-  string countAndSay(int n) {
-    
+  std::string countAndSay(int n) {
+    std::string r = "1";
+    for (int i = 0; i < n; ++i) {
+      r = _countAndSay(r);
+    }
+    return r;
   }
 };
 
 int main() {
   Solution s;
-  printf("%s\n", s.countAndSay(4));
+  printf("%s\n", s.countAndSay(4).c_str());
   return 0;
 }
