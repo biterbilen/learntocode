864888132a5c6a2d6f2792495754af746313f565
David Sun <iamslash@gmail.com>
2018-03-07 20:53:43 +0900

    update longest common prefix

--- leetcode/LongestCommonPrefix/a.cpp
+++ leetcode/LongestCommonPrefix/a.cpp
@@ -7,10 +7,27 @@
 class Solution {
 public:
   std::string longestCommonPrefix(std::vector<std::string>& strs) {
-    return "";
+    if (strs.size() == 0)
+      return "";
+    int r = 0;
+    char c = strs[0][0];
+    while (true) {
+      for (int i = 0; i < strs.size(); ++i) {
+        if (strs[i].size() <= r || strs[i][r] != c)
+          return strs[i].substr(0, r);
+      }
+      c = strs[0][++r];
+    }
+    return strs[0];
   }
 };
 
 int main() {
+  std::vector<std::string> v = {
+    "hello",
+    "hella",
+    "hey"};
+  Solution s;
+  printf("%s\n", s.longestCommonPrefix(v).c_str());
   return 0;
 }
