8a6f941bc9f4afee56c24b17ca2bec8c06465e93
dma_nada <dl_nada_dma@line.games>
2018-03-26 20:37:38 +0900

    added symmetric tree

---
+++ leetcode/SymmetricTree/a.cpp
@@ -0,0 +1,21 @@
+/* Copyright (C) 2018 by iamslash */
+
+#include <cstdio>
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+ public:
+  bool isSymmetric(TreeNode* root) {
+    return true;
+  }
+};
+
+int main() {
+  return 0;
+}
