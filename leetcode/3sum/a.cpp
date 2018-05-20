d012aabfb1f9b14282794cfc18e0fcff126a7e8e
iamslash <iamslash@gmail.com>
2018-05-20 16:16:28 +0900

    3sum ing...

--- leetcode/3sum/a.cpp
+++ leetcode/3sum/a.cpp
@@ -4,48 +4,71 @@
 #include <cstdio>
 #include <vector>
 #include <algorithm>
+#include <set>
+
+// Time limit exceeded...
+// class Solution {
+//  public:
+//   // {-1, -1, -1, -1, 0, 1, 2, -4}
+//   std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
+//     std::set<std::vector<int>> s;
+//     int n = nums.size();
+//     std::sort(nums.begin(), nums.end());
+//     for (int i = 0; i < n; ++i) {
+//       for (int j = i+1; j < n; ++j) {
+//         for (int k = j+1; k < n; ++k) {
+//           if (nums[i] + nums[j] + nums[k] == 0) {
+//             std::vector<int> a = {nums[i], nums[j], nums[k]};
+//             // r.push_back(a);
+//             s.insert(a);
+//           }
+//         }
+//       }
+//     }
+//     std::vector<std::vector<int>> r(s.begin(), s.end());
+//     return r;
+//   }
+// };
 
 class Solution {
-  public:
-    std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
-        std::vector<std::vector<int>> r;
-        int n = nums.size();
-      std::sort(nums.begin(), nums.end());
-      for (int i = 0; i < n; ++i) {
-        if (i != 0 && nums[i-1] == nums[i])
-          continue;
-        for (int j = i+1; j < n; ++j) {
-          if (j != 0 && nums[j-1] == nums[j])
-              continue;
-          for (int k = j+1; k < n; ++k) {
-            if (k != 0 && nums[k-1] == nums[k])
-                continue;
-            if (nums[i] + nums[j] + nums[k] == 0) {
-                std::vector<int> a = {nums[i], nums[j], nums[k]};
-                r.push_back(a);
-            }
+ public:
+  // {-4, -1, -1, 0, 1, 2}
+  //   i,
+  //       j,
+  //                    k
+  
+  std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
+    int n = nums.size();
+    std::vector<std::vector<int>> r;
+    std::sort(nums.begin(), nums.end());
+    for (int i = 0; i < n; ++i) {
+      for (int j = i+1; j < n; ++j) {
+        for (int k = j+1; k < n; ++k) {
+          if (nums[i] + nums[j] + nums[k] == 0) {
+            std::vector<int> a = {nums[i], nums[j], nums[k]};
+            // r.push_back(a);
+            s.insert(a);
           }
         }
       }
-      return r;
-
     }
+    return r;
+  }
 };
 
 
 int main () {
-    Solution s;
-    std::vector<int> nums = {-1, 0, 1, 2, -1, -4};
-    std::vector<std::vector<int>> r = s.threeSum(nums);
-    
-    for (auto a : r) {
-        printf("[");
-        for (int e : a) {
-            printf("%d, ", e);
-        }
-        printf("]\n");
-    }
+  Solution         s;
+  std::vector<int> nums           = {-1, 0, 1, 2, -1, -4};
+  std::vector<std::vector<int>> r = s.threeSum(nums);
 
-    return 0;
-  
+  for (auto a : r) {
+    printf("[");
+    for (int e : a) {
+      printf("%d, ", e);
+    }
+    printf("]\n");
+  }
+
+  return 0;
 }
