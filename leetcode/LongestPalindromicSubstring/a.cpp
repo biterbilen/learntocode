72c00a1e233816655ebb16910159193d7c1b48ab
iamslash <iamslash@gmail.com>
2018-06-20 12:27:01 +0900

    added Longest Palindromic Substring

---
+++ leetcode/LongestPalindromicSubstring/a.cpp
@@ -0,0 +1,16 @@
+/* Copyright (C) 2018 by iamslash */
+// https://leetcode.com/explore/interview/card/top-interview-questions-medium/103/array-and-strings/780/
+
+#include <cstdio>
+#include <string>
+
+class Solution {
+ public:
+  std::string longestPalindrome(string s) {
+    
+  }
+};
+
+int main() {
+  return 0;
+}
