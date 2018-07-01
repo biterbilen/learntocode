ef67787aead06c2e0b00dd18e9da6c1033891aa2
iamslash <iamslash@gmail.com>
2018-07-01 15:00:01 +0900

    spiral order ing...

--- leetcode/SpiralMatrix/a.cpp
+++ leetcode/SpiralMatrix/a.cpp
@@ -15,23 +15,41 @@ class Solution {
     int nx = m[0].size();
     if (nx == 0)
       return r;
-    
+
+    // for (const auto& row : m) {
+    //   for (const auto& e : row) {
+    //     printf("%d ", e);
+    //   }
+    //   printf("\n");
+    // }
     // traverse
-    int y = 0, x = 1, dy = 0, dx = 1;
-    while (true) {
-      if (x == nx && dy == 0 && dx > 0)
-        dy = 1, dx = 0, nx--;
-      else if (y == ny && dy > 0 && dx == 0)
-        dy = 0, dx = -1, ny--;
-      else if (x == 0 && dy == 0 && dx < 0)
-        dy = -1, dx = 0, nx--;
-      else if (y == 0 )
+    int sy = 0, ey = ny, sx = 0, ex = nx;  // start y, end y, start x, end x
+    int y = 0, x = 0, dy = 0, dx = 1;
+    while (sx < ex && sy < ey) {
+      printf("%d ", m[y][x]);
+      r.push_back(m[y][x]);
+
+      if (y == sy && x == sx)
+        sy++, dy = 0, dx = 1;
+      else if (x == ex && dy == 0 && dx > 0)
+        ex--, dy = 1, dx = 0;
+      else if (y == ey && dy > 0 && dx == 0)
+        ey--, dy = 0, dx = -1;
+      else if (x == sx && dy == 0 && dx < 0)
+        sx++, dy = -1, dx = 0;
+      y += dy;
+      x += dx;
     }
     return r;
   }
 };
 
 int main() {
+  // std::vector<std::vector<int>> m;
+  // m.emplace_back(std::vector<int>{1, 2, 3});
+  // m.emplace_back(std::vector<int>{3, 4, 5});
+  // m.emplace_back(std::vector<int>{6, 7, 8});
+
   std::vector<std::vector<int>> m = {{1, 2, 3},
                                      {4, 5, 6},
                                      {7, 8, 9}};
