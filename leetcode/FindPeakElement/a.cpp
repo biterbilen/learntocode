87efdc91d173f6ef912ca517f9a092b0eeef27c4
iamslash <iamslash@gmail.com>
2018-07-12 18:20:10 +0900

    accepted FindPeakElement

--- leetcode/FindPeakElement/a.cpp
+++ leetcode/FindPeakElement/a.cpp
@@ -9,15 +9,29 @@ class Solution {
  public:
   int findPeakElement(std::vector<int>& nums) {
     for (int i = 0; i < nums.size(); ++i) {
-      int a = i == 0 ? std::numeric_type<int>::max() : nums[i-1];
+      int a = i == 0 ? std::numeric_limits<int>::min() : nums[i-1];
       int b = nums[i];
-      int c = i == nums.size()-1 ? std::numeric_type<int>::max() : nums[i+1];
-      if (a < b && b > c)
-        return b;
+      int c = i == nums.size()-1 ? std::numeric_limits<int>::min() : nums[i+1];
+      // printf("%lld %lld %lld\n", a, b, c);
+      if (b == std::numeric_limits<int>::min()) {
+        if (a <= b && b >= c)
+          return i;
+      } else {
+        if (a < b && b > c)
+          return i;
+      }
     }
+    return -1;
   }
 };
 
 int main() {
+  // std::vector<int> v = {1, 2, 3, 1};
+  // std::vector<int> v = {1};
+  std::vector<int> v = {-2147483648};
+  
+  Solution s;
+  printf("%d\n", s.findPeakElement(v));
+  
   return 0;
 }
