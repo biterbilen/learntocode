760fceb60b732c7d6a91bad2e4129e199100b2a8
iamslash <iamslash@gmail.com>
2018-07-23 17:05:36 +0900

    tested bitset

--- leetcode/Pow/a.cpp
+++ leetcode/Pow/a.cpp
@@ -3,7 +3,8 @@
 
 #include <cstdio>
 #include <limits>
-
+#include <bitset>
+#include <iostream>
 
 // 0111
 class Solution {
@@ -35,6 +36,14 @@ int main() {
   printf("%lf\n", s.myPow(2.10000, 3));
   printf("%lf\n", s.myPow(2.00000, -2));
   printf("%lf\n", s.myPow(2.00000, -2147483648));
+
+  // int min = std::numeric_limits<int>::min();
+  // std::bitset<32> a(min >> 1);
+  // std::bitset<32> b((min+1) >> 1);
+  // // std::cout << a << std::endl;
+  // // std::cout << b << std::endl;
+  // printf("%s\n", a.to_string().c_str());
+  // printf("%s\n", b.to_string().c_str());
   
   return 0;
 }
