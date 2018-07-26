52c50815ebc5f63318f403f6ea861c8d2025bcd8
iamslash <iamslash@gmail.com>
2018-07-26 18:29:04 +0900

    added TaskScheduler

---
+++ TaskScheduler/a.cpp
@@ -0,0 +1,23 @@
+/* Copyright (C) 2018 by iamslash */
+// https://leetcode.com/explore/interview/card/top-interview-questions-medium/114/others/826/
+
+#include <cstdio>
+#include <vector>
+
+class Solution {
+public:
+  int leastInterval(std::vector<char>& tasks, int n) {
+    int r = 0;
+    return r;
+  }
+};
+
+int main() {
+  std::vector<char> v = {'A', 'A', 'A', 'B', 'B', 'B'};
+  int n = 2;
+  
+  Solution s;
+  printf("%d\n", s.leastInterval(v, n));
+  
+  return 0;
+}
