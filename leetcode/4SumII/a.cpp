98ceb7d4854ea8df15b8fc3a5d3892df9e0be05b
iamslash <iamslash@gmail.com>
2018-08-06 07:32:25 +0900

    4sumII ing...

--- leetcode/4SumII/a.cpp
+++ leetcode/4SumII/a.cpp
@@ -2,15 +2,31 @@
 
 #include <cstdio>
 #include <vector>
+#include <unordered_map>
 
 class Solution {
  public:
   int fourSumCount(std::vector<int>& A, std::vector<int>& B,
                    std::vector<int>& C, std::vector<int>& D) {
-    return 0;
+    int r;
+    std::unordered_map<int, int> m;
+    for (int a : A) {
+      for (int b : B) {
+        m[a+b]++;
+      }
+    }
+    for (int c : C) {
+      for (int d : D) {
+        if (m.count(-c-d))
+          r += m[-c-d];
+      }
+    }
+    return r;
   }
 };
 
+// A[i] + B[j] + C[k] + D[l] = 0
+// A[i] + B[j] = -C[k] -D[l] 
 int main() {
   std::vector<int> a = { 1,  2};
   std::vector<int> b = {-2, -1};
