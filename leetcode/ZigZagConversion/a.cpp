3b2c51ff5de4d304d159383dcb53425f078d2a85
iamslash <iamslash@gmail.com>
2018-08-09 17:16:07 +0900

    added ZigZagConversion

---
+++ leetcode/ZigZagConversion/a.cpp
@@ -0,0 +1,22 @@
+/* Copyright (C) 2018 by iamslash */
+// https://leetcode.com/problems/zigzag-conversion/description/
+
+#include <cstdio>
+#include <string>
+
+class Solution {
+ public:
+  std::string convert(std::string s, int n) {
+    std::string r;
+    return r;
+  }
+};
+
+int main() {
+  std::string ss = "PAYPALISHIRING";
+  int n = 4;
+  Solution s;
+  printf("%s\n", s.convert(ss, n).c_str());
+  
+  return 0;
+}
