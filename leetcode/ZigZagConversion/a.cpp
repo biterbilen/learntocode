8eec51f4de3d35e44bf4af605d1f211b54447226
iamslash <iamslash@gmail.com>
2018-08-10 12:18:55 +0900

    accepted ZigZagConversion

--- leetcode/ZigZagConversion/a.cpp
+++ leetcode/ZigZagConversion/a.cpp
@@ -20,35 +20,44 @@ class Solution {
       return s;
     std::vector<std::string> v(n);
     std::string r;
-    int u = n + n / 2; // unit length
-    // 
+    int u = 2*n - 2; // unit length
     //
     for (int i = 0; i < s.size(); ++i) {
       int idx  = i % u;
       if (idx >= n) {
-        idx = n - (idx - n) - 2;
+        idx = 2*n - idx - 2;
+        //idx = n - (idx - n) - 2;
       }
       v[idx].push_back(s[i]);
       // printf("i: %d, u: %d, idx: %d\n", i, u, idx);
     }
-
+    //
     for (const auto& a : v) {
       r += a;
-    }
+    }    
     return r;
   }
 };
 
 
-// A C D
-// B
+// "PAYPALISHIRING"
+// 5
+//
+// P   H
+// A  SI
+// Y I R
+// PL  IG
+// A   N
+//
 int main() {
   // std::string ss = "PAYPALISHIRING";
   // int n = 4;
   // std::string ss = "ABCDE";
   // int n = 5;
-  std::string ss = "ABCD";
-  int n = 2;
+  // std::string ss = "ABCD";
+  // int n = 2;
+  std::string ss = "PAYPALISHIRING";
+  int n = 5;
   Solution s;
   printf("%s\n", s.convert(ss, n).c_str());
   
