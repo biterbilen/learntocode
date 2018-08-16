42cbcae4c1892edb3365f944f513cf4d1bc25c1f
iamslash <iamslash@gmail.com>
2018-08-16 20:28:50 +0900

    accepted 4sum

--- leetcode/4Sum/a.cpp
+++ leetcode/4Sum/a.cpp
@@ -6,42 +6,55 @@
 #include <algorithm>
 #include <set>
 
+
 class Solution {
  public:
   std::vector<std::vector<int>> fourSum(std::vector<int>& v, int t) {
-    std::vector<std::vector<int>> r;
-    std::set<int> st(v.begin(), v.end());
-    v.clear();
-    for (auto it = st.begin(); it != st.end(); ++it)
-      v.push_back(*it);
-    
-    for (int i = 0; i < v.size(); ++i) {
-      for (int j = i+1; j < v.size(); ++j) {
-        for (int k = j+1; k < v.size(); ++k) {
-          for (int l = k+1; l < v.size(); ++l) {
-            if (v[i] + v[j] + v[k] + v[l] == t) {
-              std::vector<int> rr;
-              rr.push_back(v[i]);
-              rr.push_back(v[j]);
-              rr.push_back(v[k]);
-              rr.push_back(v[l]);
-              r.push_back(rr);
-            }
+    if (v.size() < 4)
+      return std::vector<std::vector<int>>();
+    std::set<std::vector<int>> rslt;
+    int l, r, sum;
+    std::sort(v.begin(), v.end());
+    for (int i = 0; i < v.size()-3; ++i) {
+      for (int j = i+1; j < v.size()-2; ++j) {
+        // check skip
+        if (j > i + 1 && v[j] == v[j-1])
+          continue;
+        l = j + 1;
+        r = v.size() - 1;
+        while (l < r) {
+          sum = v[i] + v[j] + v[l] + v[r];
+          if (sum == t) {
+            std::vector<int> rr = {v[i], v[j], v[l], v[r]};
+            rslt.insert(rr);
+            while (l < r && v[l] == v[l+1])
+              ++l;
+            while (l < r && v[r] == v[r-1])
+              --r;
+            ++l;
+            --r;
+          } else if (sum < t) {
+            ++l;
+          } else {
+            --r;
           }
         }
       }
     }
-    return r;
+    return std::vector<std::vector<int>>(rslt.begin(), rslt.end());
   }
 };
-
 
 int main() {
   // std::vector<int> v = {1, 0, -1, 0, -2, 2};
   // int t = 0;
-  std::vector<int> v = {-3, -2, -1, 0, 0, 1, 2, 3};
+  // std::vector<int> v = {-3, -2, -1, 0, 0, 1, 2, 3};
+  // int t = 0;
+  // std::vector<int> v = {};
+  // int t = 0;
+  std::vector<int> v = {1, 0, -1, 0, -2, 2};
   int t = 0;
-  
+
   Solution s;
   auto r = s.fourSum(v, t);
   for (const auto& rr : r) {
