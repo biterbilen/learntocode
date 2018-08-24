d95300cde6693c44ae059a6aed45e919a63ee7db
iamslash <iamslash@gmail.com>
2018-08-24 16:44:45 +0900

    CombinationSumII ing...

--- leetcode/CombinationSumII/a.cpp
+++ leetcode/CombinationSumII/a.cpp
@@ -5,25 +5,34 @@
 #include <vector>
 #include <algorithm>
 
+// 1 2 2 2 5
 class Solution {
  public:
   std::vector<std::vector<int>> m_r;
   void _combi(std::vector<int>& candi, int t,
               std::vector<int>& combi, int sum, int idx) {
+    printf("idx:%d sum:%d comb: ", idx, sum);
+    for (int i = 0; i < combi.size(); ++i)
+      printf("%d ", combi[i]);
+    printf("\n");
     // base condition
-    if (idx >= candi.size() ||
-        sum + candi[idx] > t)
+    if (idx >= candi.size())
       return;
+    // if (sum + candi[idx] > t)
+    //   return;
     if (sum + candi[idx] == t) {
       m_r.push_back(combi);
+      // combi.clear();
       return;
     }
 
     // recursion
     for (int i = idx; i < candi.size(); ++i) {
-      if (i > 0 && candi[i] == candi[i-1])
-        continue;
-      _combi(candi, t, combi, sum + candi[i], idx+1);
+      // if (i > 0 && candi[i] == candi[i-1])
+      //   continue;
+      combi.push_back(candi[i]);
+      _combi(candi, t, combi, sum + candi[i], i+1);
+      combi.pop_back();
     }
   }
   std::vector<std::vector<int>> combinationSum2(std::vector<int>& candi, int t) {
