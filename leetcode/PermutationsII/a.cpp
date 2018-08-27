fb7126e1b27f5b7853c898e19ea4a5768d88221b
iamslash <iamslash@gmail.com>
2018-08-27 20:44:00 +0900

    PermutationsII ing...

--- leetcode/PermutationsII/a.cpp
+++ leetcode/PermutationsII/a.cpp
@@ -3,32 +3,62 @@
 
 #include <cstdio>
 #include <vector>
-#include <set>
 #include <algorithm>
 
 // 1 1 2
 //
-// 1 1 2
+// 1 
 //
 // 1
 //
 // 2
 class Solution {
  public:
-  std::set<std::vector<int>> m_r;
-  void _solve(std::vector<int>& v) {
+  std::vector<std::vector<int>> m_r;
+  void _solve(int step, std::vector<int>& v, int start) {
+    for (int i = 0; i < step; ++i)
+      printf("_");
+    printf("%d | ", start);
+    for (int i = 0; i < v.size(); ++i)
+      printf("%d ", v[i]);
+    printf("\n");
     
+    int n = v.size();
+
+    // base condition
+    if (start == n - 1) {
+      m_r.push_back(v);
+      return;
+    }
+
+    // recursion
+    for (int i = start; i < n; ++i) {
+      //
+      if ((i != start) && (v[i] == v[start]))
+        continue;
+      std::swap(v[start], v[i]);
+      _solve(step+1, v, start+1);
+    }
   }
   std::vector<std::vector<int>> permuteUnique(std::vector<int>& v) {
+    if (v.empty())
+      return m_r;
+    if (v.size() == 1) {
+      m_r.push_back(v);
+      return m_r;
+    }
+      
     std::sort(v.begin(), v.end());
-    m_r.insert(v);
-    _solve(v);
-    return std::vector<std::vector<int>>(m_r.begin(), m_r.end());
+    _solve(0, v, 0);
+    return m_r;
   }
 };
 
 int main() {
-  std::vector<int> v = {1, 1, 2};
+  // std::vector<int> v = {1, 2, 3};
+  // std::vector<int> v = {1, 1, 3};
+  // std::vector<int> v = {2, 2, 1, 1};
+  std::vector<int> v = {1, 2, 2};
   
   Solution s;
   auto r = s.permuteUnique(v);
