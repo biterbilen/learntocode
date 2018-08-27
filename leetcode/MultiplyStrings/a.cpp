69db2e7d4811fa5e6f92df9cdfb67f1f25ae5bb5
iamslash <iamslash@gmail.com>
2018-08-27 21:35:13 +0900

    accepted MultiplyStrings

--- leetcode/MultiplyStrings/a.cpp
+++ leetcode/MultiplyStrings/a.cpp
@@ -3,6 +3,7 @@
 
 #include <cstdio>
 #include <string>
+#include <vector>
 
 // 1 2 3
 // 4 5 6
@@ -11,15 +12,38 @@
 class Solution {
  public:
   std::string multiply(std::string a, std::string b) {
-    if (a == "0" || b == "0")
+    int na = a.size();
+    int nb = b.size();
+    if (na == 0 || nb == 0)
       return "0";
-    std::vector<int> r;
-    for (auto i = b.rbegin(); i != b.rend(); ++i) {
-      for (auto j = a.rbegin(); j != a.rend(); ++j) {
-        
+    std::vector<int> r(na + nb, 0);
+    int pa = 0, pb = 0; // position of a, b
+    int da = 0, db = 0; // digit of a, b
+    int sum = 0, carry = 0; // sum of da and db
+    for (auto ia = na-1; ia >= 0; --ia, ++pa) {
+      carry = 0;
+      da = a[ia] - '0';
+      pb = 0;
+      for (auto ib = nb-1; ib >= 0; --ib, ++pb) {
+        db  = b[ib] - '0';
+        sum = da * db + r[pa+pb] + carry;
+        carry    = sum / 10;
+        r[pa+pb] = sum % 10;
       }
+      if (carry > 0)
+        r[pa+pb] += carry;
     }
+    
+    // check "0"
+    int i = r.size() - 1;
+    while (i >= 0 && r[i] == 0)
+      --i;
+    if (i < 0)
+      return "0";
+    
     std::string rr;
+    while (i >= 0)
+      rr += std::to_string(r[i--]);
     return rr;
   }
 };
