3d633ad7142b93652609489af5e4f5519416143c
iamslash <iamslash@gmail.com>
2018-08-30 12:02:04 +0900

    added PermutationSequence

---
+++ leetcode/PermutationSequence/a.cpp
@@ -0,0 +1,20 @@
+/* Copyright (C) 2018 by iamslash */
+// https://leetcode.com/problems/permutation-sequence/description/
+
+#include <cstdio>
+#include <string>
+
+class Solution {
+ public:
+  std::string getPermutation(int n, int k) {
+    
+  }
+};
+
+
+int main() {
+  Solution s;
+  printf("%d\n", s.getPermutation(3, 3));
+  printf("%d\n", s.getPermutation(4, 9));
+  return 0;
+}
