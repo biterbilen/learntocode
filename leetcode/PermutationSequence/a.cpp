965c87800d0a6698e1f8152678957e945c6c520e
iamslash <iamslash@gmail.com>
2018-08-30 12:23:24 +0900

    PermutationSequence ing...

--- leetcode/PermutationSequence/a.cpp
+++ leetcode/PermutationSequence/a.cpp
@@ -3,18 +3,36 @@
 
 #include <cstdio>
 #include <string>
+#include <vector>
+
+// 1 2 3
+// 1 3 2
+// 2 1 3
+// 2 3 1
+// 3 1 2
+// 3 2 1
+//
+// 3
 
 class Solution {
  public:
   std::string getPermutation(int n, int k) {
-    
+    std::string nums = "123456789";
+    std::vector<int> fcts(9, 1);
+    std::string r;
+    for (int i = 1; i < 10; ++i) {      
+      fcts[i] = fcts[i-1] * (i+1);
+    }
+    //
+    fcts[n-1] / n
+
+    return r;
   }
 };
 
 
 int main() {
   Solution s;
-  printf("%d\n", s.getPermutation(3, 3));
-  printf("%d\n", s.getPermutation(4, 9));
+  printf("%s\n", s.getPermutation(3, 3).c_str());
   return 0;
 }
