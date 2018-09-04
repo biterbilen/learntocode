3810c7c989a7f129abee2dfa5b98899ab61b7147
iamslash <iamslash@gmail.com>
2018-09-04 16:40:27 +0900

    MinimumWindowSubstring ing...

--- leetcode/MinimumWindowSubstring/a.cpp
+++ leetcode/MinimumWindowSubstring/a.cpp
@@ -5,32 +5,42 @@
 #include <string>
 #include <limits>
 #include <unordered_set>
+
+// 1set: ABC
+// 2set: 
+//
 // 0    5    0
 // ADOBECODEBANC
-// l
-//      r
+//          l   
+//             r
 class Solution {
  public:
   std::string minWindow(const std::string& s, const std::string& t) {
     int ridx;  // result index
     int rlen = std::numeric_limits<int>::max();  // result length
     int l = 0, r = 0;
-    std::unordered_multiset<int> ums(t.begin(), t.end());
+    std::unordered_multiset<int> umso(t.begin(), t.end());
+    std::unordered_multiset<int> umsb(t.begin(), t.end());
 
-    while (l <= r && r < s.size()) {
+    while (l <= r && r <= s.size()) {
+      while (l <= r && umso.find(s[l]) == umso.end())
+        ++l;
+      if (l > r)
+        break;
+      
       printf("%2d %2d | ", l, r);
-      for (int a : ums) {
+      for (int a : umsb) {
         printf("%c ", a);
       }
-      printf("\n");     
-    
-      if (ums.erase(s[r]) > 0) {
-        if (ums.size() == 0 && (r - l + 1) < rlen) {
+      printf("\n");
+
+      if (umsb.erase(s[r]) > 0 && umsb.size() == 0) {
+        if ((r - l + 1) < rlen) {
           ridx = l;
-          rlen = r - l + 1;
-          ums.insert(s[l++]);
-          ++r;
+          rlen =  r - l + 1;
         }
+        umsb.insert(s[l++]);
+        ++r;        
       } else {
         ++r;
       }
