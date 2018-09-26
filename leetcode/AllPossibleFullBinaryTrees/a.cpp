91cbdd180fd76c119fc8bec6dff1000ec8cfcb23
iamslash <iamslash@gmail.com>
2018-09-26 20:05:56 +0900

    accepted AllPossibleFullBinaryTrees

--- leetcode/AllPossibleFullBinaryTrees/a.cpp
+++ leetcode/AllPossibleFullBinaryTrees/a.cpp
@@ -3,6 +3,7 @@
 
 #include <cstdio>
 #include <vector>
+#include <unordered_map>
 
 struct TreeNode {
   int val;
@@ -26,34 +27,38 @@ struct TreeNode {
 //       0   0
 class Solution {
  public:
+  std::unordered_map<int, std::vector<TreeNode*>> m_C;
   std::vector<TreeNode*> allPossibleFBT(int N) {
-    std::vector<TreeNode*> r;
+    std::vector<TreeNode*> res;
 
     // base condition
-    if (N == 1) {
-      r.push_back(new TreeNode(0));
-      return r;
-    }
     if (N < 1 || N % 2 == 0) {
-      return r;
+      return res;
     }
+    if (N == 1) {
+      res.push_back(new TreeNode(0));
+      return res;
+    }
+    // memoization
+    if (m_C.count(N) > 0)
+      return m_C[N];
 
     // recursion
     for (int i = 1; i < N; i += 2) {
       std::vector<TreeNode*> l = allPossibleFBT(i);
       std::vector<TreeNode*> r = allPossibleFBT(N-1-i);
-      printf("%d %lu %lu\n", N, l.size(), r.size());
+      // printf("%d %lu %lu\n", N, l.size(), r.size());
 
       for (int j = 0; j < l.size(); ++j) {
         for (int k = 0; k < r.size(); ++k) {
-          printf("%d %d\n", j, k);
-          r.push_back(new TreeNode(0));
-          r.back()->left = l[j];
-          r.back()->right = r[k];
+          // printf("%lu %lu %d %d\n", l.size(), r.size(), j, k);
+          res.push_back(new TreeNode(0));
+          res.back()->left = l[j];
+          res.back()->right = r[k];
         }
       }
     }
-    return r;
+    return m_C[N] = res;
   }
 };
 
