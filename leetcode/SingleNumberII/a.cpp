d369bb6dcedd2d5b852dfeb12f310d69b437392a
iamslash <iamslash@gmail.com>
2018-09-27 22:58:49 +0900

    accepted LinkedListCycleII

---
+++ leetcode/SingleNumberII/a.cpp
@@ -0,0 +1,20 @@
+// Copyright (C) 2018 by iamslash
+// https://leetcode.com/problems/single-number-ii/description/
+
+#include <cstdio>
+#include <vector>
+
+class Solution {
+ public:
+  int singleNumber(std::vector<int>& nums) {
+    
+  }
+};
+
+int main() {
+  std::vector<int> v = {2, 2, 2, 3};
+
+  Solution s;
+  printf("%d\n", s.singleNumber(v));
+  return 0;
+}
