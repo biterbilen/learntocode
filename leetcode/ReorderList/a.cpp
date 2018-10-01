4a199e6af64f1f14c9dc37bf00975fa4f935d099
iamslash <iamslash@gmail.com>
2018-10-01 17:30:32 +0900

    accepted ReorderList

--- leetcode/ReorderList/a.cpp
+++ leetcode/ReorderList/a.cpp
@@ -8,44 +8,65 @@ struct ListNode {
   explicit ListNode(int x, ListNode* p) : val(x), next(p) {}
 };
 
-//   h
-// a 1 2 4 3
-//   b
+void printl(ListNode* h) {
+  while (h) {
+    printf("%d ", h->val);
+    h = h->next;
+  }
+  printf("\n");
+}
+
+
+// h     
+// 1 2 3   5 4
 //     p
-//       q
+//           q
 class Solution {
  public:
   void reorderList(ListNode* h) {
     if (!h || !h->next)
       return;
 
-    ListNode* p = h, *q = h->next, *r = h;
+    ListNode* p = h, *q = h->next, *r;
+
     // separate 2 lists
     while (q && q->next) {
       p = p->next;
       q = q->next->next;
     }
-    ListNode* h2 = p->next;
-    q = p->next;
-    p->next = NULL;
+    
+    ListNode* i = p->next;
+    q = i;
+    
     // reverse 2nd list
+    p->next = NULL;
+    p = NULL;    
     while (q) {
+      i = q;
       r = q->next;
       q->next = p;
       p = q;
-      q = r;
+      q = r;      
     }
+
     // merge 2 lists
     p = h;
-    q = h2;
-    while (p) {
+    q = i;
+    while (q) {
       r = p->next;
-      p = p->next = q;
+      p->next = q;
+      p = r;
+      r = q->next;
+      q->next = p;
       q = r;
     }
   }
 };
 
 int main() {
+  ListNode* h = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+  Solution s;
+  s.reorderList(h);
+  printl(h);
   return 0;
 }
