83546e1e8fec093331109764f1d09f7523f456b6
iamslash <iamslash@gmail.com>
2018-10-03 12:11:51 +0900

    accepted FriendsCirls

--- leetcode/FriendCircles/a.cpp
+++ leetcode/FriendCircles/a.cpp
@@ -7,69 +7,56 @@
 
 class Solution {
  public:
-  std::vector<std::vector<int>> m_M;  // visited
-  std::vector<std::vector<int>> m_V;  // visited
+  std::vector<std::vector<int>> m_M;  // Matrix
+  std::vector<bool> m_V;  // visited
   int m_N;
-  void dfs(int y, int x) {
+  void dfs(int i) {
     // printf("%d %d\n", y, x);
-    m_V[y][x] = 1;
-
-    int dy[4] = {-1, 0, 1, 0};
-    int dx[4] = {0, 1, 0, -1};
-    for (int i = 0; i < 4; ++i) {
-      int ny = y + dy[i];
-      int nx = x + dx[i];
-      if (ny < 0 || ny >= m_h || nx < 0 || nx >= m_w ||
-          m_M[ny][nx] == 0 || m_V[ny][nx] == 1)
+    m_V[i] = true;
+    for (int j = 0; j < m_N; ++j) {
+      if (m_V[j] || m_M[i][j] == 0)
         continue;
-      dfs(ny, nx);
+      dfs(j);
     }
   }
+
   int findCircleNum(std::vector<std::vector<int>>& M) {
     if (M.empty() || M[0].empty())
       return 0;
     m_M = M;
-    m_V = M;
     m_N = M.size();
-    
-    for (int y = 0; y < m_h; ++y) {
-      for (int x = 0; x < m_w; ++x) {
-        m_V[y][x] = 0;
-      }
-    }
+    m_V.resize(m_N, false);
 
     int rslt = 0;
-    for (int y = 0; y < m_h; ++y) {
-      for (int x = 0; x < m_w; ++x) {
-        if (M[y][x] == 0 || m_V[y][x] == 1)
-          continue;
+    for (int i = 0; i < m_N; ++i) {
+      if (m_V[i])
+        continue;
           // printf("%d %d\n", y, x);
-          dfs(y, x);
-          rslt++;
-      }
+      dfs(i);
+      rslt++;
     }
     return rslt;
   }
 };
 
 int main() {
-  // std::vector<std::vector<int>> M = {
-  //   {1, 1, 0},
-  //   {1, 1, 0},
-  //   {0, 0, 1},
-  // };
+  std::vector<std::vector<int>> M = {
+    {1, 1, 0},
+    {1, 1, 0},
+    {0, 0, 1},
+  };
   // std::vector<std::vector<int>> M = {
   //   {1, 1, 0},
   //   {1, 1, 1},
   //   {0, 1, 1},
   // };
-  std::vector<std::vector<int>> M = {
-    {1, 0, 0, 1},
-    {0, 1, 1, 0},
-    {0, 1, 1, 1},
-    {1, 0, 1, 1},
-  };
+  // std::vector<std::vector<int>> M = {
+  //   {1, 0, 0, 1},
+  //   {0, 1, 1, 0},
+  //   {0, 1, 1, 1},
+  //   {1, 0, 1, 1},
+  // };
   Solution s;
-  printf("%d\n", s.findCircleNum(M));  
+  printf("%d\n", s.findCircleNum(M));
   return 0;
 }
