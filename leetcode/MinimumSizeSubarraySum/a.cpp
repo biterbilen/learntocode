82110c770d122064407d1d9d21995d8b466ea2c9
iamslash <iamslash@gmail.com>
2018-10-21 20:32:06 +0900

    Minimum Size Subarray Sum ing...

--- leetcode/MinimumSizeSubarraySum/a.cpp
+++ leetcode/MinimumSizeSubarraySum/a.cpp
@@ -9,27 +9,34 @@ class Solution {
     if (nums.empty())
       return 0;
     // sliding window
-    int l = 0, r = 0, rslt = 0, sum = nums[0];
+    int l = 0, r = 0, rslt = std::numeric_limits<int>::max(), sum = 0;
     while (l <= r && r < nums.size()) {
+      sum += nums[r];
+      printf("l: %d r: %d sum: %d\n", l, r, sum);
       if (sum < s) {
         ++r;
       } else if (sum > s) {
+        sum -= nums[l];
+        sum -= nums[r];
         ++l;
       } else {
-        rslt = std::min(rslt, r - l);
+        rslt = std::min(rslt, r - l + 1);
+        sum -= nums[l];
         ++l;
+        ++r;
       }
     }
-    return rslt;
+    return rslt == std::numeric_limits<int>::max() ? 0 : rslt;
   }
 };
 
 int main() {
-  std::vector<int> nums = {2, 3, 1, 2, 4, 3};
+  // std::vector<int> nums = {2, 3, 1, 2, 4, 3};
+  std::vector<int> nums = {1, 2, 3, 4, 5};
 
   Solution sln;
 
-  printf("%d\n", sln.minSubArrayLen(7, nums));
+  printf("%d\n", sln.minSubArrayLen(11, nums));
   
   return 0;
 }
